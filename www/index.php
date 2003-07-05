cd28c292716164687fd9a17b0a5e7075278fee64
Tobias Blomberg <sm0svx@users.sourceforge.net>
2003-07-05 20:54:22 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@2 f8843c6b-282e-0410-804b-c0d5895947d2

---
+++ www/index.php
@@ -0,0 +1,28 @@
+<?php include("header.inc"); ?>
+
+Welcome to the home of SvxLink. The goal iof this project is to
+implement a well documented C++ library to build EchoLink applications
+around. The only application that exists right now is Qtel -- the Qt
+EchoLink client.
+<P/>
+EchoLink is an amateur radio invention (well actually it is just a
+modified verison of Internet telephony) to link radio transcievers together
+over the Internet. You must have an amateur radio license to use
+it. The original EchoLink software can be found at
+<A href="http://www.echolink.org/">http://www.echolink.org/</A>. However,
+this software only supports the Windows operating system and it is
+closed source. SvxLink is released under the GPL license. 
+<P/>
+For now, Qtel is only a EchoLink client program. It does not have the
+sysop mode. That is, it is not yet possible to connect a transciver
+and act as a link.
+<P/>
+Qtel is known to work under these operating systems (I really hope that
+this list will grow in time):
+<UL>
+  <LI>Red Hat Linux 7.3 and 9</LI>
+</UL>
+<P/>
+
+<?php include("footer.inc"); ?>
+
