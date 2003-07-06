460ff95e9d24e3fd27a00da6da6ae7d18ac4742f
Tobias Blomberg <sm0svx@users.sourceforge.net>
2003-07-06 21:05:14 +0000

    Initial revision
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@9 f8843c6b-282e-0410-804b-c0d5895947d2

---
+++ src/qtel/translations/qtel_sv.ts
@@ -0,0 +1,533 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>ComDialog</name>
+    <message>
+        <source>INFO</source>
+        <translation>INFO</translation>
+    </message>
+    <message>
+        <source>Connected to </source>
+        <translation>Ansluten till </translation>
+    </message>
+    <message>
+        <source>Connecting to </source>
+        <translation>Ansluter till </translation>
+    </message>
+    <message>
+        <source>Disconnected
+</source>
+        <translation>Nedkopplad</translation>
+    </message>
+    <message>
+        <source>Qtel Error</source>
+        <translation>Qtel fel</translation>
+    </message>
+    <message>
+        <source>Station data not found in directory server.
+Can&apos;t create connection to</source>
+        <translation>Kunde inte hitta stationsdatan i listservern.
+Kan ej skapa uppkoppling till</translation>
+    </message>
+    <message>
+        <source>Could not create connection to station</source>
+        <translation>Kunde inte ansluta till station</translation>
+    </message>
+    <message>
+        <source>Could not open audio device</source>
+        <translation>Kunde inte komma åt ljudkortet</translation>
+    </message>
+</context>
+<context>
+    <name>ComDialogBase</name>
+    <message>
+        <source>Communication Dialog</source>
+        <translation>Kommunikationsdialog</translation>
+    </message>
+    <message>
+        <source>Station Information</source>
+        <translation>Stationsinformation</translation>
+    </message>
+    <message>
+        <source>?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Description:</source>
+        <translation>Beskrivning:</translation>
+    </message>
+    <message>
+        <source>IP-address:</source>
+        <translation>IP-adress:</translation>
+    </message>
+    <message>
+        <source>Status:</source>
+        <translation>Status:</translation>
+    </message>
+    <message>
+        <source>Time:</source>
+        <translation>Tid:</translation>
+    </message>
+    <message>
+        <source>Chat</source>
+        <translation type="obsolete"></translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation type="obsolete">Anslut</translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation type="obsolete">Koppla ner</translation>
+    </message>
+    <message>
+        <source>PTT</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Chat/Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Connect</source>
+        <translation>&amp;Anslut</translation>
+    </message>
+    <message>
+        <source>&amp;Disconnect</source>
+        <translation>&amp;Koppla ner</translation>
+    </message>
+    <message>
+        <source>RX</source>
+        <translation>RX</translation>
+    </message>
+    <message>
+        <source>TX</source>
+        <translation>TX</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Namn:</translation>
+    </message>
+</context>
+<context>
+    <name>IncomingConnection</name>
+    <message>
+        <source>Incoming connection</source>
+        <translation type="obsolete"></translation>
+    </message>
+    <message>
+        <source>Incoming connection from</source>
+        <translation type="obsolete"></translation>
+    </message>
+    <message>
+        <source>Do you want to accept it ?</source>
+        <translation type="obsolete"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Logging off from directory server...</source>
+        <translation>Loggar ut från listserver...</translation>
+    </message>
+    <message>
+        <source>Station list has been refreshed</source>
+        <translation>Stationslistan har blivit uppdaterad</translation>
+    </message>
+    <message>
+        <source>Refreshing station list...</source>
+        <translation>Uppdaterar stationslista...</translation>
+    </message>
+    <message>
+        <source>Getting calls from directory server...</source>
+        <translation>Hämtar stationslista från listserver...</translation>
+    </message>
+    <message>
+        <source>Connect request from</source>
+        <translation type="obsolete">Uppkopplingsförsök från</translation>
+    </message>
+    <message>
+        <source>Add to bookmarks</source>
+        <translation>Lägg till bokmärke</translation>
+    </message>
+    <message>
+        <source>Remove from bookmarks</source>
+        <translation>Tag bort bokmärke</translation>
+    </message>
+    <message>
+        <source>Add named station...</source>
+        <translation>Lägg till namngiven station...</translation>
+    </message>
+    <message>
+        <source>Qtel - Add station...</source>
+        <translation>Qtel - Lägg till station...</translation>
+    </message>
+    <message>
+        <source>Enter callsign of the station to add</source>
+        <translation>Mata in anropssignalen för den
+station du vill lägga till</translation>
+    </message>
+    <message>
+        <source>Ctrl+Q</source>
+        <comment>fileQuitAction</comment>
+        <translation>Ctrl+A</translation>
+    </message>
+    <message>
+        <source>Ctrl+R</source>
+        <comment>directoryRefreshAction</comment>
+        <translation type="obsolete">Ctrl+P</translation>
+    </message>
+    <message>
+        <source>Ctrl+B</source>
+        <comment>directoryBusyAction</comment>
+        <translation>Ctrl+U</translation>
+    </message>
+    <message>
+        <source>F9</source>
+        <comment>directoryRefreshAction</comment>
+        <translation type="obsolete">F9</translation>
+    </message>
+    <message>
+        <source>F5</source>
+        <comment>directoryRefreshAction</comment>
+        <translation>F5</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowBase</name>
+    <message>
+        <source>qtel</source>
+        <translation type="obsolete"></translation>
+    </message>
+    <message>
+        <source>Server Messages</source>
+        <translation type="obsolete">Servermeddelanden</translation>
+    </message>
+    <message>
+        <source>Station Browser</source>
+        <translation>Stationsbläddrare</translation>
+    </message>
+    <message>
+        <source>Explorer</source>
+        <translation>Utforskare</translation>
+    </message>
+    <message>
+        <source>Conferences</source>
+        <translation>Konferenser</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>Länkar</translation>
+    </message>
+    <message>
+        <source>Repeaters</source>
+        <translation>Relästationer</translation>
+    </message>
+    <message>
+        <source>Stations</source>
+        <translation>Stationer</translation>
+    </message>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Bokmärken</translation>
+    </message>
+    <message>
+        <source>Station</source>
+        <translation>Station</translation>
+    </message>
+    <message>
+        <source>Location/Description</source>
+        <translation>Plats/Beskrivning</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Time</source>
+        <translation>Tid</translation>
+    </message>
+    <message>
+        <source>IP-address</source>
+        <translation>IP-adress</translation>
+    </message>
+    <message>
+        <source>&amp;File</source>
+        <translation>&amp;Arkiv</translation>
+    </message>
+    <message>
+        <source>&amp;Settings</source>
+        <translation>&amp;Inställningar</translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation>&amp;Hjälp</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Ångra</translation>
+    </message>
+    <message>
+        <source>&amp;Undo</source>
+        <translation>&amp;Ångra</translation>
+    </message>
+    <message>
+        <source>Redo</source>
+        <translation>Ångra ångra</translation>
+    </message>
+    <message>
+        <source>&amp;Redo</source>
+        <translation>Å&amp;ngra ångra</translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation>Klipp ut</translation>
+    </message>
+    <message>
+        <source>&amp;Cut</source>
+        <translation>&amp;Klipp ut</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+    <message>
+        <source>C&amp;opy</source>
+        <translation>K&amp;opiera</translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation>Klistra in</translation>
+    </message>
+    <message>
+        <source>&amp;Paste</source>
+        <translation>K&amp;listra in</translation>
+    </message>
+    <message>
+        <source>Find</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source>&amp;Find...</source>
+        <translation>&amp;Sök</translation>
+    </message>
+    <message>
+        <source>Contents</source>
+        <translation>Innehåll</translation>
+    </message>
+    <message>
+        <source>&amp;Contents...</source>
+        <translation>I&amp;nnehåll...</translation>
+    </message>
+    <message>
+        <source>Index</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>&amp;Index...</source>
+        <translation>&amp;Index...</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>&amp;About...</source>
+        <translation>&amp;Om...</translation>
+    </message>
+    <message>
+        <source>About Qtel</source>
+        <translation>Om Qtel</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Avsluta</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;Avsluta</translation>
+    </message>
+    <message>
+        <source>Quit the Qtel application</source>
+        <translation>Avsluta Qtel</translation>
+    </message>
+    <message>
+        <source>Qtel Settings...</source>
+        <translation>Inställningar för Qtel...</translation>
+    </message>
+    <message>
+        <source>&amp;Qtel Settings...</source>
+        <translation>&amp;Inställningar för Qtel</translation>
+    </message>
+    <message>
+        <source>Open the Qtel settings dialog</source>
+        <translation>Öppna inställningsdialogen för Qtel</translation>
+    </message>
+    <message>
+        <source>Qtel v</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>- Qt EchoLink client.
+</source>
+        <translation>- Qt EchoLink-klient.
+</translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation>Författare</translation>
+    </message>
+    <message>
+        <source>&amp;Directory</source>
+        <translation>&amp;Stationslista</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>&amp;Refresh</source>
+        <translation>U&amp;ppdatera</translation>
+    </message>
+    <message>
+        <source>Refresh station list</source>
+        <translation>Uppdatera stationslista</translation>
+    </message>
+    <message>
+        <source>Qtel - the Qt EchoLink Client</source>
+        <translation>Qtel - Qt EchoLink Klient</translation>
+    </message>
+    <message>
+        <source>Busy</source>
+        <translation>Upptagen</translation>
+    </message>
+    <message>
+        <source>&amp;Busy</source>
+        <translation>&amp;Upptagen</translation>
+    </message>
+    <message>
+        <source>View as busy</source>
+        <translation>Visa som upptagen</translation>
+    </message>
+    <message>
+        <source>View as busy in directory server</source>
+        <translation>Visa som upptagen i listservern</translation>
+    </message>
+    <message>
+        <source>Messages</source>
+        <translation>Meddelanden</translation>
+    </message>
+    <message>
+        <source>Local Time</source>
+        <translation type="obsolete">Lokal tid</translation>
+    </message>
+    <message>
+        <source>Local time</source>
+        <translation>Lokal tid</translation>
+    </message>
+    <message>
+        <source>Incoming connections</source>
+        <translation>Inkommande uppkopplingar</translation>
+    </message>
+    <message>
+        <source>Accept</source>
+        <translation>Acceptera</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>Rensa</translation>
+    </message>
+    <message>
+        <source>Callsign</source>
+        <translation>Anropssignal</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <source>Qtel: Settings</source>
+        <translation>Qtel: Inställningar</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>User Info</source>
+        <translation>Användarinfo</translation>
+    </message>
+    <message>
+        <source>User Information</source>
+        <translation>Användarinformation</translation>
+    </message>
+    <message>
+        <source>Info
+Message</source>
+        <translation>Informations-
+meddelande</translation>
+    </message>
+    <message>
+        <source>Location</source>
+        <translation>Plats</translation>
+    </message>
+    <message>
+        <source>Callsign</source>
+        <translation>Anropssignal</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Lösenord</translation>
+    </message>
+    <message>
+        <source>Retype password</source>
+        <translation>Repetera lösenord</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>Directory Server</source>
+        <translation>Listserver</translation>
+    </message>
+    <message>
+        <source>Directory Server Settings</source>
+        <translation>Listserverinställningar</translation>
+    </message>
+    <message>
+        <source>Server</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>The hostname or IP address of the EchoLink directory server to use</source>
+        <translation>Namn eller IP-adress på den EchoLink listserver som skall användas</translation>
+    </message>
+    <message>
+        <source>Refresh time</source>
+        <translation>Uppdateringsintervall</translation>
+    </message>
+    <message>
+        <source>min</source>
+        <translation>min</translation>
+    </message>
+    <message>
+        <source>Time, in minutes, between station list refreshes</source>
+        <translation>Tid, i minuter, mellan uppdatering av stationslistan</translation>
+    </message>
+    <message>
+        <source>Register as busy on program startup</source>
+        <translation>Registrera som upptagen vid programstart</translation>
+    </message>
+    <message>
+        <source>Start as busy</source>
+        <translation>Starta som upptagen</translation>
+    </message>
+</context>
+</TS>
