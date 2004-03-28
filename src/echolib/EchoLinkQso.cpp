2318b84adb86539d79b812282865fea9d7b474a2
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-03-28 09:35:58 +0000

    Added missing include "cassert".
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@62 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/echolib/EchoLinkQso.cpp
+++ src/echolib/EchoLinkQso.cpp
@@ -40,7 +40,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
-#include <cassert>
 
 
 /****************************************************************************
