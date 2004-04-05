9b557b908e7405a2079dc9cf8aa5ba8d4f16b1d2
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-04-05 22:21:48 +0000

    Rewrote the main page to indicate the new goal of the project.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@151 f8843c6b-282e-0410-804b-c0d5895947d2

--- www/index.php
+++ www/index.php
@@ -1,9 +1,15 @@
 <?php include("header.inc"); ?>
 
-Welcome to the home of SvxLink. The goal of this project is to
-implement a well documented C++ library to build EchoLink applications
-around. The only application that exists right now is Qtel -- the Qt
-EchoLink client.
+Welcome to the home of SvxLink. 
+The SvxLink project aim to develop a flexible general purpose voice services
+system for ham radio use. The svxlink server consists of a core that handles the
+connection to the tranciever. The transciever audio is connected to the PC
+through the sound card and the PTT is controlled by a pin in the serial port.
+The voice services are loaded into the core as plugins called modules in SvxLink
+lingo. Examples of existing voice services are: Help - a help system, Parrot - a
+module that plays back everything you say and EchoLink - connect to other
+EchoLink stations. The project also includes an EchoLink client GUI application
+(Qtel).
 <P/>
 EchoLink is an amateur radio invention (well actually it is just a
 modified verison of Internet telephony) to link radio transcievers together
@@ -13,14 +19,15 @@ it. The original EchoLink software can be found at
 this software only supports the Windows operating system and it is
 closed source. SvxLink is released under the GPL license. 
 <P/>
-For now, Qtel is only a EchoLink client program. It does not have the
-sysop mode. That is, it is not yet possible to connect a transciver
-and act as a link.
+Qtel is only a EchoLink client program. It does not have the
+sysop mode. That is, it can not be connected to a transciver
+and act as a link. For the latter, use the svxlink server.
 <P/>
-Qtel is known to work under these operating systems (I really hope that
+SvxLink is known to work under these operating systems (I really hope that
 this list will grow in time):
 <UL>
-  <LI>Red Hat Linux 7.3 and 9</LI>
+  <LI>Red Hat Linux 9</LI>
+  <LI>Fedora Core 1</LI>
 </UL>
 <P/>
 
