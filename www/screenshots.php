10911a5acb426e6ea637491ad5cd62138e858bc4
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-04-07 21:23:16 +0000

    Added a page header. Better layout of the images.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@157 f8843c6b-282e-0410-804b-c0d5895947d2

--- www/screenshots.php
+++ www/screenshots.php
@@ -1,28 +1,30 @@
 <?php include("header.inc"); ?>
 
+<H2>Qtel Screenshots</H2>
+
 <TABLE width="90%" cellpadding="10">
   <TR align="center">
     <TD>
       <A href="images/qtel-mainwindow.png">
         <IMG src="images/qtel-mainwindow-small.png" border="0"
              alt="Main Window Screenshot"/><BR/>
-        The main window
+        Qtel main window
       </A>
     </TD>
     <TD>
       <A href="images/qtel-settings.png">
         <IMG src="images/qtel-settings-small.png" border="0"
              alt="Settings Dialog Screenshot"/><BR/>
-        The settings dialog
+        Qtel settings dialog
       </A>
     </TD>
   </TR>
   <TR align="center">
-    <TD>
+    <TD colspan="2">
       <A href="images/qtel-comdialog.png">
         <IMG src="images/qtel-comdialog-small.png" border="0"
              alt="Communications Dialog Screenshot"/><BR/>
-        The communications dialog
+        Qtel communications dialog
       </A>
     </TD>
   </TR>
