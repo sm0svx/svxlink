05d0a398496857af2aa08ac15e0eb52186aee21d
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-07-30 10:19:36 +0000

    Initial revision.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@312 f8843c6b-282e-0410-804b-c0d5895947d2

---
+++ src/template.h
@@ -0,0 +1,154 @@
+/**
+@file	 Template.h
+@brief   A_brief_description_for_this_file
+@author  Tobias Blomberg / SM0SVX
+@date	 2004-08-
+
+A_detailed_description_for_this_file
+
+\verbatim
+<A brief description of the program or library this file belongs to>
+Copyright (C) 2004  Tobias Blomberg / SM0SVX
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
+/** @example Template_demo.cpp
+An example of how to use the Template class
+*/
+
+
+#ifndef TEMPLATE_INCLUDED
+#define TEMPLATE_INCLUDED
+
+
+/****************************************************************************
+ *
+ * System Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Project Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Local Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Forward declarations
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Namespace
+ *
+ ****************************************************************************/
+
+namespace MyNameSpace
+{
+
+
+/****************************************************************************
+ *
+ * Forward declarations of classes inside of the declared namespace
+ *
+ ****************************************************************************/
+
+  
+
+/****************************************************************************
+ *
+ * Defines & typedefs
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Exported Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Class definitions
+ *
+ ****************************************************************************/
+
+/**
+@brief	A_brief_class_description
+@author Tobias Blomberg
+@date   2004-03-
+
+A_detailed_class_description
+
+\include Template_demo.cpp
+*/
+class Template
+{
+  public:
+    /**
+     * @brief 	Default constuctor
+     */
+    Template(void);
+  
+    /**
+     * @brief 	Destructor
+     */
+    ~Template(void);
+  
+    /**
+     * @brief 	A_brief_member_function_description
+     * @param 	param1 Description_of_param1
+     * @return	Return_value_of_this_member_function
+     */
+    
+  protected:
+    
+  private:
+    
+};  /* class Template */
+
+
+} /* namespace */
+
+#endif /* TEMPLATE_INCLUDED */
+
+
+
+/*
+ * This file has not been truncated
+ */
+
