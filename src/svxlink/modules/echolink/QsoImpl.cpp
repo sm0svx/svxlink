975127a97be856fdbebf3d9ee2adced8bcc67543
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-07-30 10:06:59 +0000

    Initial revision.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@306 f8843c6b-282e-0410-804b-c0d5895947d2

---
+++ src/svxlink/modules/echolink/QsoImpl.cpp
@@ -0,0 +1,432 @@
+/**
+@file	 QsoImpl.cpp
+@brief   Data for one EchoLink Qso.
+@author  Tobias Blomberg / SM0SVX
+@date	 2004-06-02
+
+This file contains a class that implementes the things needed for one
+EchoLink Qso.
+
+\verbatim
+<A brief description of the program or library this file belongs to>
+Copyright (C) 2003 Tobias Blomberg / SM0SVX
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
+
+
+/****************************************************************************
+ *
+ * System Includes
+ *
+ ****************************************************************************/
+
+#include <cassert>
+#include <sigc++/bind.h>
+
+
+/****************************************************************************
+ *
+ * Project Includes
+ *
+ ****************************************************************************/
+
+#include <AsyncConfig.h>
+//#include <Module.h>
+
+#include <MsgHandler.h>
+#include <AudioPacer.h>
+
+
+/****************************************************************************
+ *
+ * Local Includes
+ *
+ ****************************************************************************/
+
+#include "ModuleEchoLink.h"
+#include "QsoImpl.h"
+
+
+
+/****************************************************************************
+ *
+ * Namespaces to use
+ *
+ ****************************************************************************/
+
+using namespace std;
+using namespace Async;
+using namespace EchoLink;
+
+
+
+/****************************************************************************
+ *
+ * Defines & typedefs
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Local class definitions
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Prototypes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Exported Global Variables
+ *
+ ****************************************************************************/
+
+
+
+
+/****************************************************************************
+ *
+ * Local Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Public member functions
+ *
+ ****************************************************************************/
+
+
+QsoImpl::QsoImpl(const Async::IpAddress& ip, ModuleEchoLink *module)
+  : Qso(ip), module(module), msg_handler(0), msg_pacer(0), init_ok(false),
+    reject_qso(false), last_message(""), last_info_msg("")
+{
+  assert(module != 0);
+  
+  Config &cfg = module->cfg();
+  const string &cfg_name = module->cfgName();
+  
+  string local_callsign;
+  if (!cfg.getValue(cfg_name, "CALLSIGN", local_callsign))
+  {
+    cerr << "*** ERROR: Config variable " << cfg_name << "/CALLSIGN not set\n";
+    return;
+  }
+  setLocalCallsign(local_callsign);
+  
+  string sysop_name;
+  if (!cfg.getValue(cfg_name, "SYSOPNAME", sysop_name))
+  {
+    cerr << "*** ERROR: Config variable " << cfg_name
+      	 << "/SYSOPNAME not set\n";
+    return;
+  }
+  setLocalName(sysop_name);
+  
+  string description;
+  if (!cfg.getValue(cfg_name, "DESCRIPTION", description))
+  {
+    cerr << "*** ERROR: Config variable " << cfg_name
+      	 << "/DESCRIPTION not set\n";
+    return;
+  }
+  setLocalInfo(description);
+  
+  string sound_base_dir;
+  if (!cfg.getValue(module->logicName(), "SOUNDS", sound_base_dir))
+  {
+    cerr << "*** ERROR: Config variable " << cfg_name
+      	 << "/SOUNDS not set\n";
+    return;
+  }
+  
+  msg_handler = new MsgHandler(sound_base_dir, 8000);
+  
+  msg_pacer = new AudioPacer(8000, 160*4, 500);
+  msg_handler->writeAudio.connect(slot(msg_pacer, &AudioPacer::audioInput));
+  msg_handler->allMsgsWritten.connect(
+      	  slot(msg_pacer, &AudioPacer::flushAllAudio));
+  msg_pacer->audioInputBufFull.connect(
+      	  slot(msg_handler, &MsgHandler::writeBufferFull));
+  msg_pacer->allAudioFlushed.connect(
+      	  slot(this, &QsoImpl::allRemoteMsgsWritten));
+  msg_pacer->audioOutput.connect(slot(this, &Qso::sendAudio));
+  
+  Qso::infoMsgReceived.connect(slot(this, &QsoImpl::onInfoMsgReceived));
+  Qso::chatMsgReceived.connect(slot(this, &QsoImpl::onChatMsgReceived));
+  Qso::stateChange.connect(slot(this, &QsoImpl::onStateChange));
+  Qso::isReceiving.connect(slot(this, &QsoImpl::onIsReceiving));
+  Qso::audioReceived.connect(slot(this, &QsoImpl::onAudioReceived));
+
+  
+  init_ok = true;
+  
+} /* QsoImpl::QsoImpl */
+
+
+QsoImpl::~QsoImpl(void)
+{
+  delete msg_pacer;
+  delete msg_handler;
+} /* QsoImpl::~QsoImpl */
+
+
+bool QsoImpl::initOk(void)
+{
+  return Qso::initOk() && init_ok;
+} /* QsoImpl::initOk */
+
+
+int QsoImpl::sendAudio(short *buf, int len)
+{
+    /* FIXME: Buffer audio until the message has been written. */
+  if (!msg_handler->isWritingMessage())
+  {
+    len = Qso::sendAudio(buf, len);
+  }
+  
+  return len;
+  
+} /* QsoImpl::sendAudio */
+
+
+bool QsoImpl::accept(void)
+{
+  cout << "Accepting connection from " << remoteCallsign() << endl;
+  bool success = Qso::accept();
+  if (success)
+  {
+    msg_handler->playMsg("EchoLink", "greeting");
+  }
+  
+  return success;
+  
+} /* QsoImpl::accept */
+
+
+void QsoImpl::reject(void)
+{
+  cout << "Rejecting connection from " << remoteCallsign() << endl;
+  reject_qso = true;
+  bool success = Qso::accept();
+  if (success)
+  {
+    sendChatData("The connection was rejected");
+    msg_handler->playMsg("EchoLink", "reject_connection");
+    msg_handler->playSilence(1000);
+  }
+} /* QsoImpl::reject */
+
+
+
+/****************************************************************************
+ *
+ * Protected member functions
+ *
+ ****************************************************************************/
+
+
+/*
+ *------------------------------------------------------------------------
+ * Method:    
+ * Purpose:   
+ * Input:     
+ * Output:    
+ * Author:    
+ * Created:   
+ * Remarks:   
+ * Bugs:      
+ *------------------------------------------------------------------------
+ */
+
+
+
+
+
+
+/****************************************************************************
+ *
+ * Private member functions
+ *
+ ****************************************************************************/
+
+
+/*
+ *----------------------------------------------------------------------------
+ * Method:    
+ * Purpose:   
+ * Input:     
+ * Output:    
+ * Author:    
+ * Created:   
+ * Remarks:   
+ * Bugs:      
+ *----------------------------------------------------------------------------
+ */
+void QsoImpl::allRemoteMsgsWritten(void)
+{
+  flushAudioSendBuffer();
+  if (reject_qso)
+  {
+    disconnect();
+  }
+} /* QsoImpl::allRemoteMsgsWritten */
+
+
+/*
+ *----------------------------------------------------------------------------
+ * Method:    onInfoMsgReceived
+ * Purpose:   Called by the EchoLink::Qso object when an info message is
+ *    	      received from the remote station.
+ * Input:     msg - The received message
+ * Output:    None
+ * Author:    Tobias Blomberg / SM0SVX
+ * Created:   2004-03-07
+ * Remarks:   
+ * Bugs:      
+ *----------------------------------------------------------------------------
+ */
+void QsoImpl::onInfoMsgReceived(const string& msg)
+{
+  if (msg != last_info_msg)
+  {
+    cout << "--- EchoLink info message received from " << remoteCallsign()
+	 << " ---" << endl
+	 << msg << endl;
+    last_info_msg = msg;
+  }  
+} /* onInfoMsgReceived */
+
+
+/*
+ *----------------------------------------------------------------------------
+ * Method:    onChatMsgReceived
+ * Purpose:   Called by the EchoLink::Qso object when a chat message is
+ *    	      received from the remote station.
+ * Input:     msg - The received message
+ * Output:    None
+ * Author:    Tobias Blomberg / SM0SVX
+ * Created:   2004-07-29
+ * Remarks:   
+ * Bugs:      
+ *----------------------------------------------------------------------------
+ */
+void QsoImpl::onChatMsgReceived(const string& msg)
+{
+  cout << "--- EchoLink chat message received from " << remoteCallsign()
+       << " ---" << endl
+       << msg << endl;
+  chatMsgReceived(this, msg);
+} /* onChatMsgReceived */
+
+
+/*
+ *----------------------------------------------------------------------------
+ * Method:    onStateChange
+ * Purpose:   Called by the EchoLink::Qso object when the connection state
+ *    	      changes.
+ * Input:     state - The state new state of the QSO
+ * Output:    None
+ * Author:    Tobias Blomberg / SM0SVX
+ * Created:   2004-03-07
+ * Remarks:   
+ * Bugs:      
+ *----------------------------------------------------------------------------
+ */
+void QsoImpl::onStateChange(Qso::State state)
+{
+  cout << remoteCallsign() << ": EchoLink QSO state changed to ";
+  switch (state)
+  {
+    case Qso::STATE_DISCONNECTED:
+      cout << "DISCONNECTED\n";
+      if (!reject_qso)
+      {
+	module->spellCallsign(remoteCallsign());
+	module->playMsg("disconnected");
+	module->playSilence(500);
+      }
+      destroyMe(this);
+      break;
+    case Qso::STATE_CONNECTING:
+      cout << "CONNECTING\n";
+      break;
+    case Qso::STATE_CONNECTED:
+      cout << "CONNECTED\n";
+      if (!reject_qso)
+      {
+	module->playMsg("connected");
+	if (isRemoteInitiated())
+	{
+      	  module->spellCallsign(remoteCallsign());
+	}
+      }
+      break;
+    case Qso::STATE_BYE_RECEIVED:
+      cout << "BYE_RECEIVED\n";
+      break;
+    default:
+      cout << "???\n";
+      break;
+  }
+} /* onStateChange */
+
+
+/*
+ *----------------------------------------------------------------------------
+ * Method:    onIsReceiving
+ * Purpose:   Called by the EchoLink::Qso object to indicate whether the
+ *    	      remote station is transmitting or not.
+ * Input:     is_receiving  - true=remote station is transmitting
+ *    	      	      	      false=remote station is not transmitting
+ * Output:    None
+ * Author:    Tobias Blomberg / SM0SVX
+ * Created:   2004-03-07
+ * Remarks:   
+ * Bugs:      
+ *----------------------------------------------------------------------------
+ */
+void QsoImpl::onIsReceiving(bool is_receiving)
+{
+  //cerr << "EchoLink receiving" << (is_receiving ? "true" : "false") << endl;
+  isReceiving(this, is_receiving);
+} /* onIsReceiving */
+
+
+int QsoImpl::onAudioReceived(short *samples, int count)
+{
+  return audioReceived(this, samples, count);
+} /* QsoImpl::onAudioReceived */
+
+
+
+/*
+ * This file has not been truncated
+ */
+
---
+++ src/svxlink/modules/echolink/QsoImpl.h
@@ -0,0 +1,265 @@
+/**
+@file	 QsoImpl.h
+@brief   Data for one EchoLink Qso.
+@author  Tobias Blomberg / SM0SVX
+@date	 2004-06-02
+
+This file contains a class that implementes the things needed for one
+EchoLink Qso.
+
+\verbatim
+<A brief description of the program or library this file belongs to>
+Copyright (C) 2004  Tobias Blomberg / SM0SVX
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
+/** @example Template_demo.cpp
+An example of how to use the Template class
+*/
+
+
+#ifndef QSO_IMPL_INCLUDED
+#define QSO_IMPL_INCLUDED
+
+
+/****************************************************************************
+ *
+ * System Includes
+ *
+ ****************************************************************************/
+
+#include <string>
+
+
+/****************************************************************************
+ *
+ * Project Includes
+ *
+ ****************************************************************************/
+
+#include <EchoLinkQso.h>
+
+
+/****************************************************************************
+ *
+ * Local Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Forward declarations
+ *
+ ****************************************************************************/
+
+namespace Async
+{
+  class Config;
+};
+
+
+/****************************************************************************
+ *
+ * Namespace
+ *
+ ****************************************************************************/
+
+//namespace MyNameSpace
+//{
+
+
+/****************************************************************************
+ *
+ * Forward declarations of classes inside of the declared namespace
+ *
+ ****************************************************************************/
+
+class MsgHandler;
+class AudioPacer;
+class ModuleEchoLink;
+
+
+/****************************************************************************
+ *
+ * Defines & typedefs
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Exported Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Class definitions
+ *
+ ****************************************************************************/
+
+/**
+@brief	Implements the things needed for one EchoLink QSO.
+@author Tobias Blomberg
+@date   2004-06-02
+
+A class that implementes the things needed for one EchoLink Qso.
+*/
+class QsoImpl : public EchoLink::Qso
+{
+  public:
+    /**
+     * @brief 	Default constuctor
+     */
+    QsoImpl(const Async::IpAddress& ip, ModuleEchoLink *module);
+  
+    /**
+     * @brief 	Destructor
+     */
+    virtual ~QsoImpl(void);
+  
+    /**
+     * @brief 	Check that the initialization went ok
+     * @return	Returns \em true if the initialization was ok or \em false on
+     *	      	failure
+     *
+     * This function should be called after creating a new Qso object to make
+     * sure everything went well.
+     */
+    bool initOk(void);
+
+    /**
+     * @brief 	Send audio to the remote station
+     * @param 	buf A buffer containing 16 bit samples to send
+     * @param 	len The length, in samples, of the buffer to send
+     * @return	Returns the number of samples written
+     *
+     * This function is used to send audio to the remote station. This
+     * reimplemented version of EchoLink::Qso::sendAudio throws away
+     * the samples if a message is being played to the remote station.
+     */
+    int sendAudio(short *buf, int len);
+    
+    /**
+     * @brief 	Accept an incoming connection
+     * @return	Returns \em true if the connect message was sent successfully or
+     *	      	\em false on failure
+     *
+     * Use this function to accept an incoming connection. Incoming connections
+     * are signalled through the EchoLink::Dispatcher. When an incoming
+     * connection has been received, a Qso object should be created and this
+     * function should be called to accept the connection. Be sure to check that
+     * a valid callsign has connected. At least if the EchoLink node is
+     * connected to a radio transmitter.
+     *
+     * The difference between the connect and accept functions are that the
+     * accept function goes right into the connected state. The remote station
+     * is assumed to be present. This might not be true in some strange cases.
+     * In such a strange case, the connection will timeout after a while.
+     */
+    bool accept(void);
+    
+    /**
+     * @brief 	Reject the connection
+     */
+    void reject(void);
+
+
+    /**
+     * @brief A signal that is emitted when the connection state changes
+     * @param state The new connection state
+     * @param qso The QSO object
+     */
+    //SigC::Signal2<void, Qso::State, QsoImpl*> stateChange;
+    
+    /**
+     * @brief A signal that is emitted when a station info message is received
+     * @param qso The QSO object
+     * @param msg The received message
+     */
+    //SigC::Signal2<void, QsoImpl*, const std::string&> infoMsgReceived;
+    
+    /**
+     * @brief A signal that is emitted when a chat message is received
+     * @param qso The QSO object
+     * @param msg The received chat message
+     */
+    SigC::Signal2<void, QsoImpl*, const std::string&> chatMsgReceived;
+    
+    /**
+     * @brief A signal that is emitted when the audio receive state changes
+     * @param qso The QSO object
+     * @param is_receiving  Is \em true when audio is being received and
+     *                      \em false when not
+     * @note This signal can be used to control a reception indicator
+     */
+    SigC::Signal2<void, QsoImpl*, bool> isReceiving;
+    
+    /**
+     * @brief A signal that is emitted when an audio datagram has been received
+     * @param qso The QSO object
+     * @param buf A pointer to the buffer that contains the audio
+     * @param len The number of samples in the buffer
+     */
+    SigC::Signal3<int, QsoImpl*, short*, int> audioReceived;
+    
+    SigC::Signal1<void, QsoImpl*> destroyMe;
+    
+    
+    /**
+     * @brief 	A_brief_member_function_description
+     * @param 	param1 Description_of_param1
+     * @return	Return_value_of_this_member_function
+     */
+
+    
+  protected:
+    
+  private:
+    ModuleEchoLink    	*module;
+    MsgHandler	      	*msg_handler;
+    AudioPacer       	*msg_pacer;
+    bool      	      	init_ok;
+    bool      	      	reject_qso;
+    std::string       	last_message;
+    std::string       	last_info_msg;
+    
+    void allRemoteMsgsWritten(void);
+    void onInfoMsgReceived(const std::string& msg);
+    void onChatMsgReceived(const std::string& msg);
+    void onStateChange(Qso::State state);
+    void onIsReceiving(bool is_receiving);
+    int onAudioReceived(short *samples, int count);
+
+};  /* class QsoImpl */
+
+
+//} /* namespace */
+
+#endif /* QSO_IMPL_INCLUDED */
+
+
+
+/*
+ * This file has not been truncated
+ */
+
