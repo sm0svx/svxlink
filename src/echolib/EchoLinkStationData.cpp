5435e6c2e4be38b2e8406d67bb24b4f467a57d0d
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-07-31 22:02:26 +0000

    Now also compiles under Mac OS X.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@323 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/echolib/EchoLinkStationData.cpp
+++ src/echolib/EchoLinkStationData.cpp
@@ -38,6 +38,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <cstring>
 #include <cstdlib>
 
+#include <sys/types.h>
+
 
 /****************************************************************************
  *
--- src/echolib/rtpacket.c
+++ src/echolib/rtpacket.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netinet/ip.h>
+#include <arpa/inet.h>
 
 #include "rtp.h"
 #include "rtpacket.h"
