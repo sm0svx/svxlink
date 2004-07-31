99d3dc3fa537330e8d91c80e305b30d2fcf6988e
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-07-31 21:43:06 +0000

    Now compiles under Mac OS X.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@320 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/async/core/AsyncApplication.cpp
+++ src/async/core/AsyncApplication.cpp
@@ -35,6 +35,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ****************************************************************************/
 
+#include <sys/types.h>
 #include <sys/select.h>
 #include <stdlib.h>
 
--- src/async/core/AsyncIpAddress.cpp
+++ src/async/core/AsyncIpAddress.cpp
@@ -36,6 +36,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ****************************************************************************/
 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
