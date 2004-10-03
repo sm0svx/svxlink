7ba11ffef62bdc7bdd205d1fbe0e887497315bc5
Tobias Blomberg <sm0svx@users.sourceforge.net>
2004-10-03 20:14:49 +0000

    Typo...
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@413 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/async/core/AsyncTcpServer.cpp
+++ src/async/core/AsyncTcpServer.cpp
@@ -43,7 +43,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <iostream>
 #include <stdio.h>
 #include <algorithm>
-#include <assert>
+#include <cassert>
 
 
 /****************************************************************************
