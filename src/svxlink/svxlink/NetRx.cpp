9b2e5c1cc4246a2f42e1366d5941a217c498445e
Tobias Blomberg <sm0svx@users.sourceforge.net>
2006-04-14 18:48:13 +0000

    Moved the NetRxMsg.h include file to the svxlink dir.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@846 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/svxlink/svxlink/NetRx.cpp
+++ src/svxlink/svxlink/NetRx.cpp
@@ -43,7 +43,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ****************************************************************************/
 
 #include <AsyncConfig.h>
-#include <NetRxMsg.h>
 
 
 /****************************************************************************
@@ -53,6 +52,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ****************************************************************************/
 
 #include "NetRx.h"
+#include "NetRxMsg.h"
 
 
 
