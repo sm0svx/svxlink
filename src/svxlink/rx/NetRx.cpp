cedc56b3cd0e590d3f71876d20d7d2fff4699151
Tobias Blomberg <sm0svx@users.sourceforge.net>
2006-04-14 22:27:48 +0000

    Forgot the NetRxMsg.h file.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@851 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/svxlink/rx/NetRx.cpp
+++ src/svxlink/rx/NetRx.cpp
@@ -51,8 +51,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ****************************************************************************/
 
-#include "NetRx.h"
 #include "NetRxMsg.h"
+#include "NetRx.h"
 
 
 
