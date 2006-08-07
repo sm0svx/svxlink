357bc1edbae4c90af536106a5a8566af99993cf7
Tobias Blomberg <sm0svx@users.sourceforge.net>
2006-08-07 15:14:12 +0000

    New classes: Async::AudioSelector and Async::AudioPassthrough.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@926 f8843c6b-282e-0410-804b-c0d5895947d2

---
+++ src/async/audio/AsyncAudioSelector.cpp
@@ -0,0 +1,233 @@
+/**
+@file	 AsyncAudioSelector.cpp
+@brief   This file contains a class that is used to select one of many audio
+      	 streams.
+@author  Tobias Blomberg / SM0SVX
+@date	 2006-08-01
+
+\verbatim
+Async - A library for programming event driven applications
+Copyright (C) 2006 Tobias Blomberg / SM0SVX
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
+
+
+/****************************************************************************
+ *
+ * System Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Project Includes
+ *
+ ****************************************************************************/
+
+#include <AsyncAudioPassthrough.h>
+
+
+/****************************************************************************
+ *
+ * Local Includes
+ *
+ ****************************************************************************/
+
+#include "AsyncAudioSelector.h"
+
+
+
+/****************************************************************************
+ *
+ * Namespaces to use
+ *
+ ****************************************************************************/
+
+using namespace std;
+using namespace Async;
+
+
+
+/****************************************************************************
+ *
+ * Defines & typedefs
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Local class definitions
+ *
+ ****************************************************************************/
+
+static class NullSource : public AudioSource
+{
+  public:
+    void resumeOutput(void) {}
+    void allSamplesFlushed(void) {}
+    
+} null_source;
+
+
+class Async::AudioSelector::Branch : public AudioPassthrough
+{
+  public:
+    Branch(AudioSelector *selector, AudioSource *source)
+    {
+      assert(registerSource(source));
+    }
+    
+}; /* class Branch */
+
+
+
+/****************************************************************************
+ *
+ * Prototypes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Exported Global Variables
+ *
+ ****************************************************************************/
+
+
+
+
+/****************************************************************************
+ *
+ * Local Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Public member functions
+ *
+ ****************************************************************************/
+
+AudioSelector::AudioSelector(void)
+{
+  setHandler(&null_source);
+} /* AudioSelector::AudioSelector */
+
+
+AudioSelector::~AudioSelector(void)
+{
+  BranchMap::iterator it;
+  for (it = branch_map.begin(); it != branch_map.end(); ++it)
+  {
+    delete (*it).second;
+  }
+} /* AudioSelector::~AudioSelector */
+
+
+void AudioSelector::addSource(Async::AudioSource *source)
+{
+  assert(branch_map.find(source) == branch_map.end());
+  branch_map[source] = new Branch(this, source);
+} /* AudioSelector::addSource */
+
+
+void AudioSelector::selectSource(AudioSource *source)
+{
+  if (source == handler())
+  {
+    return;
+  }
+  
+  clearHandler();
+
+  if (source == 0)
+  {
+    setHandler(&null_source);
+    return;
+  }
+  
+  assert(branch_map.find(source) != branch_map.end());
+  Branch *branch = branch_map[source];
+  setHandler(branch);
+} /* AudioSelector::selectSource */
+
+
+
+/****************************************************************************
+ *
+ * Protected member functions
+ *
+ ****************************************************************************/
+
+
+/*
+ *------------------------------------------------------------------------
+ * Method:    
+ * Purpose:   
+ * Input:     
+ * Output:    
+ * Author:    
+ * Created:   
+ * Remarks:   
+ * Bugs:      
+ *------------------------------------------------------------------------
+ */
+
+
+
+
+
+
+/****************************************************************************
+ *
+ * Private member functions
+ *
+ ****************************************************************************/
+
+
+/*
+ *----------------------------------------------------------------------------
+ * Method:    
+ * Purpose:   
+ * Input:     
+ * Output:    
+ * Author:    
+ * Created:   
+ * Remarks:   
+ * Bugs:      
+ *----------------------------------------------------------------------------
+ */
+
+
+
+
+
+
+
+/*
+ * This file has not been truncated
+ */
+
---
+++ src/async/audio/AsyncAudioSelector.h
@@ -0,0 +1,163 @@
+/**
+@file	 AsyncAudioSelector.h
+@brief   This file contains a class that is used to select one of many audio
+      	 streams.
+@author  Tobias Blomberg / SM0SVX
+@date	 2006-08-01
+
+\verbatim
+Async - A library for programming event driven applications
+Copyright (C) 2004-2005  Tobias Blomberg / SM0SVX
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
+#ifndef ASYNC_AUDIO_SELECTOR_INCLUDED
+#define ASYNC_AUDIO_SELECTOR_INCLUDED
+
+
+/****************************************************************************
+ *
+ * System Includes
+ *
+ ****************************************************************************/
+
+#include <map>
+
+
+/****************************************************************************
+ *
+ * Project Includes
+ *
+ ****************************************************************************/
+
+#include <AudioSource.h>
+
+
+/****************************************************************************
+ *
+ * Local Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Forward declarations
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Namespace
+ *
+ ****************************************************************************/
+
+namespace Async
+{
+
+
+/****************************************************************************
+ *
+ * Forward declarations of classes inside of the declared namespace
+ *
+ ****************************************************************************/
+
+  
+
+/****************************************************************************
+ *
+ * Defines & typedefs
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Exported Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Class definitions
+ *
+ ****************************************************************************/
+
+/**
+@brief	This class is used to select one of many audio streams
+@author Tobias Blomberg / SM0SVX
+@date   2006-08-01
+
+This class is used to select one of many incoming audio streams. Incoming
+samples on non-selected branches will be thrown away.
+*/
+class AudioSelector : public AudioSource
+{
+  public:
+    /**
+     * @brief 	Default constuctor
+     */
+    AudioSelector(void);
+  
+    /**
+     * @brief 	Destructor
+     */
+    ~AudioSelector(void);
+  
+    /**
+     * @brief 	Add an audio source to the selector
+     * @param 	source The audio source to add
+     */
+    void addSource(AudioSource *source);
+
+    /**
+     * @brief 	Select one of the previously added audio sources
+     * @param 	source The audio source to select. 0 = none selected.
+     */
+    void selectSource(AudioSource *source);
+    
+    
+  protected:
+    
+  private:
+    class Branch;
+    typedef std::map<Async::AudioSource *, Branch *> BranchMap;
+    
+    BranchMap branch_map;
+    
+    AudioSelector(const AudioSelector&);
+    AudioSelector& operator=(const AudioSelector&);
+    
+};  /* class AudioSelector */
+
+
+} /* namespace */
+
+#endif /* ASYNC_AUDIO_SELECTOR_INCLUDED */
+
+
+
+/*
+ * This file has not been truncated
+ */
+
