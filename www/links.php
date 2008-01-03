c2508861c62b3f3252938cdc52b6cb95253d7af2
Tobias Blomberg <sm0svx@users.sourceforge.net>
2008-01-03 11:51:59 +0000

    Small changes.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@1123 f8843c6b-282e-0410-804b-c0d5895947d2

--- www/links.php
+++ www/links.php
@@ -9,7 +9,7 @@ This page contain some SvxLink related links.
 <UL>
   <LI><A href="http://www.echolink.org/">EchoLink</A> - The original EchoLink software</LI>
   <LI><A href="http://www.hacem.org/#svxlink">WB0RXX/Tim's SvxLink page</A> - Extra modules for SvxLink and schematics for a TRX interface</LI>
-  <LI><A href="http://cqinet.sourceforge.net/">CQiNet</A> - Home of EchoLinux and Thebridge applications</LI>
+  <LI><A href="http://cqinet.sourceforge.net/">CQiNet</A> - Home of the EchoLinux and Thebridge applications</LI>
 </UL>
 
 <?php include("footer.inc"); ?>
