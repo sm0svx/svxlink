3b85528356988c8a92f950da230f8fbd596814e5
Tobias Blomberg <sm0svx@users.sourceforge.net>
2008-06-28 21:20:34 +0000

    Added a link to the S54S interface.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@1382 f8843c6b-282e-0410-804b-c0d5895947d2

--- www/links.php
+++ www/links.php
@@ -10,6 +10,7 @@ This page contain some SvxLink related links.
   <LI><A href="http://www.echolink.org/">EchoLink</A> - The original EchoLink software</LI>
   <LI><A href="http://www.hacem.org/#svxlink">WB0RXX/Tim's SvxLink page</A> - Extra modules for SvxLink and schematics for a TRX interface</LI>
   <LI><A href="http://cqinet.sourceforge.net/">CQiNet</A> - Home of the EchoLinux and Thebridge applications</LI>
+  <LI><A href="http://lea.hamradio.si/~s57nan/ham_radio/">S54S Transceiver to PC interface</A> - A fully isolated interface featuring a hardware DTMF decoder</LI>
 </UL>
 
 <?php include("footer.inc"); ?>
