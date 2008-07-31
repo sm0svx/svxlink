bb5c81817afbd1fa44f4155dc7d2bdd903f013a0
Tobias Blomberg <sm0svx@users.sourceforge.net>
2008-07-31 15:19:02 +0000

    Updated the installation instruction for Fedora 8.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@1415 f8843c6b-282e-0410-804b-c0d5895947d2

--- www/install-f8.php
+++ www/install-f8.php
@@ -4,7 +4,7 @@
 ?>
 
 <H2>Installation instructions for Fedora 8</H2>
-This instruction was written for the 080102 release of SvxLink.
+This instruction was written for the 080730 release of SvxLink.
 <P>
 The easiest way to install SvxLink is to use the
 <A href="install_f8.sh">installation script</A>.
@@ -17,10 +17,10 @@ If that fails for some reason, follow the manual description below.
 Start by downloading the latest versions of the following SvxLink RPM:s from the
 SourceForge
 <A href="http://sourceforge.net/project/showfiles.php?group_id=84813">download site</A>:
-<A href="http://downloads.sourceforge.net/svxlink/libasync-0.15.0-1.fc8.i386.rpm">libasync</A>,
-<A href="http://downloads.sourceforge.net/svxlink/echolib-0.12.1-1.fc8.i386.rpm">echolib</A>,
-<A href="http://downloads.sourceforge.net/svxlink/svxlink-server-0.9.0-1.fc8.i386.rpm">svxlink-server</A> and/or
-<A href="http://downloads.sourceforge.net/svxlink/qtel-0.10.1-1.fc8.i386.rpm">qtel</A>.
+<A href="http://downloads.sourceforge.net/svxlink/libasync-0.16.1-1.fc8.i386.rpm">libasync</A>,
+<A href="http://downloads.sourceforge.net/svxlink/echolib-0.13.0-2.fc8.i386.rpm">echolib</A>,
+<A href="http://downloads.sourceforge.net/svxlink/svxlink-server-0.10.1-1.fc8.i386.rpm">svxlink-server</A> and/or
+<A href="http://downloads.sourceforge.net/svxlink/qtel-0.11.1-1.fc8.i386.rpm">qtel</A>.
 <P>
 Import the public key that the SvxLink RPM:s are signed with, if you've not
 already done so.
@@ -33,7 +33,7 @@ have SvxLink installed, use "yum localupdate" instead. The advantage of using
 yum to install the RPM:s is that all packages that SvxLink depend on will be
 automatically downloaded and installed.
 <PRE>
-yum localinstall libasync-0.15.0-1.fc8.i386.rpm echolib-0.12.1-1.fc8.i386.rpm qtel-0.10.1-1.fc8.i386.rpm svxlink-server-0.9.0-1.fc8.i386.rpm
+yum localinstall libasync-0.16.1-1.fc8.i386.rpm echolib-0.13.0-2.fc8.i386.rpm qtel-0.11.1-1.fc8.i386.rpm svxlink-server-0.10.1-1.fc8.i386.rpm
 </PRE>
 <P>
 To make the SvxLink server start on boot, use:
