e4bb3815f0a02d9bfe2029bd18d60a9056b4cff2
Tobias Blomberg <sm0svx@users.sourceforge.net>
2010-01-08 12:23:04 +0000

    Bugfix: The BEACON_INTERVAL configuration variable was not correctly handled.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@1709 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/svxlink/modules/echolink/LocationInfo.cpp
+++ src/svxlink/modules/echolink/LocationInfo.cpp
@@ -335,7 +335,7 @@ bool LocationInfo::parseStationHW(const Async::Config &cfg, const string &name)
     success = false;
   }
 
-  int interval = 0;
+  int interval = 60;
   int max = numeric_limits<int>::max();
   if (!cfg.getValue(name, "BEACON_INTERVAL", 10, max, interval, true))
   {
@@ -345,7 +345,7 @@ bool LocationInfo::parseStationHW(const Async::Config &cfg, const string &name)
   }
   else
   {
-    loc_cfg.interval = 60 * 1000 * interval;
+    loc_cfg.interval = 1000 * interval;
   }
 
   loc_cfg.range = calculateRange(loc_cfg);
