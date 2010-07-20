1e0fe890f397d694f0cbcc24259ead37cc959670
Tobias Blomberg <sm0svx@users.sourceforge.net>
2010-07-20 19:53:43 +0000

    Improved the splitStr function so that it can now accept any container value type that support streaming and not just strings.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@1848 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/misc/common.h
+++ src/misc/common.h
@@ -34,6 +34,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ****************************************************************************/
 
+#include <sstream>
 
 
 /****************************************************************************
@@ -100,41 +101,100 @@ namespace SvxLink
  *
  ****************************************************************************/
 
+/**
+ * @brief   Set a value of any type supporting streaming from a string
+ * @param   val Reference to the value to set
+ * @param   str The sting to set the value from
+ * @returns Returns \em true if the operation succeeded or \em false if not
+ *
+ * This function use the streaming operator to set the outgoing value from the
+ * incoming string so any type that support the streaming operator will work.
+ * All characters in the string must be consumed for the parser to indicate
+ * success.
+ */
+template <typename ValueType>
+static bool setValueFromString(ValueType &val, const std::string &str)
+{
+  std::istringstream ss(str);
+  ss >> std::noskipws >> val >> std::ws;
+  return !ss.fail() && ss.eof();
+} /* setValueFromString */
+
+
+/**
+ * @brief   Set a value of any type supporting streaming from a string
+ * @param   val Reference to the value to set
+ * @param   str The sting to set the value from
+ * @returns Returns \em true if the operation succeeded or \em false if not
+ *
+ * This function is a specialization for std::string of the more general
+ * template function. It just copies the incoming string to the outgoing value.
+ * If streaming were to be used for strings, white space would be
+ * unconditionally ignored.
+ */
+template <>
+static bool setValueFromString(std::string &val, const std::string &str)
+{
+  val = str;
+  return true;
+} /* setValueFromString */
+
+
+/**
+ * @brief   Split a string using a set of delimiters and put the tokens in a
+ *          container
+ * @param   L       The container to put the tokens into. Any container that
+ *                  supports the clear, size and push_back methods can be used.
+ * @param   seq     The string to split
+ * @param   delims  The set of delimiters used to split the string. Be sure to
+ *                  include any white space characters that you want to ignore.
+ * @returns Returns the number of tokens stored in the container
+ *
+ * This is a template function that is used to split strings into tokens,
+ * storing the tokens into a container. The container can be of any type that
+ * supports the clear, size and push_back methods (e.g. vector, list, deque).
+ *
+ * The container value type may be any that supports setting a value by
+ * streaming to it (e.g. std::string, int, float, char etc).
+ *
+ * If a parsed value is malformed, in a way so that it cannot be parsed into
+ * the given container value type, it will be ignored.
+ *
+ * Empty values will not be stored.
+ */
 template <class Container>
-int splitStr(Container &L, const std::string &seq, const std::string &delims)
+static typename Container::size_type splitStr(Container &L,
+                                              const std::string &seq,
+                                              const std::string &delims)
 {
   L.clear();
 
-  std::string str;
   std::string::size_type pos = 0;
   std::string::size_type len = seq.size();
   while (pos < len)
   {
-      // Init/clear the token buffer
-    str = "";
-
       // remove any delimiters including optional (white)spaces
     while ((delims.find(seq[pos]) != std::string::npos) && (pos < len))
     {
       pos++;
     }
 
-      // leave if @eos
-    if (pos == len)
-    {
-      return L.size();
-    }
-
       // Save token data
+    std::string str;
     while ((delims.find(seq[pos]) == std::string::npos) && (pos < len))
     {
       str += seq[pos++];
     }
 
-      // put valid str buffer into the supplied list
+      // Put valid str buffer into the supplied list. If the specified string
+      // does not match the value type, it is skipped.
     if (!str.empty())
     {
-      L.push_back(str);
+      typename Container::value_type val;
+      if (setValueFromString(val, str))
+      {
+        L.push_back(val);
+      }
     }
   }
 
