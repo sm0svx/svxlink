f800a9853879d2f82964179e27021546b3789bf8
Tobias Blomberg <sm0svx@users.sourceforge.net>
2010-11-28 11:12:43 +0000

    Extended the standard help reader with a feature that allows storing the help sound clips for subcommands in separate files. If at least one file named help_subcmd*.{wav,raw,gsm} exist, the sound clip "sub commands are" is first played and then the help text for all subcommands prefixed with its command number are played. The command number should be encoded in the file name, e.g. help_subcmd32.wav for sub command 32.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@1897 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/svxlink/svxlink/Module.tcl
+++ src/svxlink/svxlink/Module.tcl
@@ -45,7 +45,25 @@ proc timeout {module_name} {
 # Executed when playing of the help message for a module has been requested.
 #
 proc play_help {module_name} {
+  global basedir;
+
   playMsg $module_name "help";
+
+  set subcmds [glob -nocomplain "$basedir/$module_name/help_subcmd*.{wav,raw,gsm}"]
+  if {[llength $subcmds] > 0} {
+    playSilence 500
+    playMsg "Core" "sub_commands_are"
+    foreach subcmd [lsort $subcmds] {
+      if [regexp {^.*/help_subcmd(\d+)([ABCD*#]*)\.} $subcmd -> number chars] {
+        puts $subcmd
+        playSilence 200
+        playNumber $number
+        spellWord $chars
+        playSilence 200
+        playFile $subcmd
+      }
+    }
+  }
 }
 
 
