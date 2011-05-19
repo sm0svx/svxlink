2bbffc89ac31fd22316b11859dbab97783513816
Steve DH1DM <steve-k@users.sourceforge.net>
2011-05-19 19:43:19 +0000

    Fixed a very small but important bug in RepeaterLogic that caused a SvxLink repeater to re-open on interference shutdown.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/branches/new_audio_pipe@1998 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/async/audio/AsyncAudioMixer.h
+++ src/async/audio/AsyncAudioMixer.h
@@ -46,7 +46,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include <AsyncAudioSource.h>
 #include <AsyncAudioFifo.h>
-#include <SigCAudioSource.h>
 
 
 /****************************************************************************
