473af8a34cab997ee5e0f0faad2b020c3f0b1fb4
Steve DH1DM <steve-k@users.sourceforge.net>
2011-05-29 05:18:32 +0000

    Removed SampleFifo.[cpp|h] from the repository because it was not used anymore for a long time and is not portable to the new audio framework
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/branches/new_audio_pipe@1999 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/async/audio/AsyncAudioMixer.cpp
+++ src/async/audio/AsyncAudioMixer.cpp
@@ -51,7 +51,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ****************************************************************************/
 
 #include "AsyncAudioMixer.h"
-#include "AsyncAudioFifo.h"
 #include "AsyncAudioReader.h"
 
 
