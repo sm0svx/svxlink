df8c2da2d56af268b31181ccdbad9ee06a43899f
Tobias Blomberg <sm0svx@users.sourceforge.net>
2011-08-04 17:02:14 +0000

    - Patch from Steve to eliminate the ToneDurationDetector. The timing is now   done directly in the ToneDetector instead.
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@2035 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/svxlink/trx/ToneDetector.cpp
+++ src/svxlink/trx/ToneDetector.cpp
@@ -5,8 +5,8 @@
 @date	 2003-04-15
 
 \verbatim
-<A brief description of the program or library this file belongs to>
-Copyright (C) 2004-2005  Tobias Blomberg / SM0SVX
+SvxLink - A Multi Purpose Voice Services System for Ham Radio Use
+Copyright (C) 2004-2011  Tobias Blomberg / SM0SVX
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -79,8 +79,8 @@ using namespace Async;
  ****************************************************************************/
 
 #define DEFAULT_PEAK_THRESH	10.0	// 10dB
-#define DEFAULT_DET_DELAY	3
-#define DEFAULT_UNDET_DELAY	3
+#define DEFAULT_DET_POSITIVES	3
+#define DEFAULT_GAP_NEGATIVES	2
 #define DEFAULT_ENERGY_THRESH	0.1f
 
 
@@ -136,10 +136,10 @@ using namespace Async;
  * Bugs:      
  *------------------------------------------------------------------------
  */
-ToneDetector::ToneDetector(float tone_hz, float width_hz)
-  : samples_left(0), is_activated(false), tone_fq(tone_hz), block_len(0),
-    det_delay_left(DEFAULT_DET_DELAY), undet_delay_left(0),
-    peak_thresh(DEFAULT_PEAK_THRESH), energy_thresh(DEFAULT_ENERGY_THRESH)
+ToneDetector::ToneDetector(float tone_hz, float width_hz, int det_delay_ms)
+  : tone_fq(tone_hz), block_len(0), samples_left(0),
+    is_activated(false), last_active(false), peak_thresh(DEFAULT_PEAK_THRESH),
+    stable_count(0)
 {
   // The Goertzel algorithm is just a recursive way to evaluate the DFT at a
   // single frequency. For maximum detection reliability, the bandwidth will
@@ -147,34 +147,80 @@ ToneDetector::ToneDetector(float tone_hz, float width_hz)
   // As a side effect, the detection bandwidth is slightly narrowed, which
   // however is acceptable for the current use cases (CTCSS, 1750Hz, etc..).
 
-  /* Adjust block length to minimize the DFT error */
+    // Adjust block length to minimize the DFT error
   block_len = lrintf(INTERNAL_SAMPLE_RATE * ceilf(tone_hz / width_hz) / tone_hz);
-  /* Center frequency */
   goertzelInit(&center, tone_hz, INTERNAL_SAMPLE_RATE);
-  /* Lower frequency */
   goertzelInit(&lower, tone_hz - 2 * width_hz, INTERNAL_SAMPLE_RATE);
-  /* Upper frequency */
   goertzelInit(&upper, tone_hz + 2 * width_hz, INTERNAL_SAMPLE_RATE);
-  /* Hamming window */  
+
+    // Set up Hamming window coefficients
   for (int i = 0; i < block_len; i++)
   {
     window_table.push_back(
       0.54 - 0.46 * cosf(2.0f * M_PI * i / (block_len - 1)));
   }
-  /* Point to the first table entry */
-  win = window_table.begin();
-  /* Load sample counter */
-  samples_left = block_len;
 
+  setDetectionDelay(det_delay_ms);
+  setGapDelay(0);
+  reset();
 } /* ToneDetector::ToneDetector */
 
 
 void ToneDetector::reset(void)
 {
   is_activated = false;
-  det_delay_left = DEFAULT_DET_DELAY;
-  undet_delay_left = 0;
+  last_active = false;
+  stable_count = 0;
+  samples_left = block_len;
+
+    // Point to the first windowing table entry
+  win = window_table.begin();
+
+    // Reset Goertzel filters
+  center.v2 = center.v3 = 0.0f;
+  lower.v2 = lower.v3 = 0.0f;
+  upper.v2 = upper.v3 = 0.0f;
 } /* ToneDetector::reset */
+
+
+void ToneDetector::setDetectionDelay(int new_delay_ms)
+{
+  if (new_delay_ms > 0)
+  {
+    int block_time = block_len * 1000 / INTERNAL_SAMPLE_RATE;
+    det_delay = (new_delay_ms-1) / block_time + 1;
+  }
+  else
+  {
+    det_delay = DEFAULT_DET_POSITIVES;
+  }
+} /* ToneDetector::setDetectionDelay */
+
+
+int ToneDetector::detectionDelay(void) const
+{
+  return det_delay * block_len * 1000 / INTERNAL_SAMPLE_RATE;
+}
+
+
+void ToneDetector::setGapDelay(int new_delay_ms)
+{
+  if (new_delay_ms > 0)
+  {
+    int block_time = block_len * 1000 / INTERNAL_SAMPLE_RATE;
+    gap_delay = (new_delay_ms-1) / block_time + 1;
+  }
+  else
+  {
+    gap_delay = DEFAULT_GAP_NEGATIVES;
+  }
+} /* ToneDetector::setGapDelay */
+
+
+int ToneDetector::gapDelay(void) const
+{
+  return gap_delay * block_len * 1000 / INTERNAL_SAMPLE_RATE;
+}
 
 
 int ToneDetector::writeSamples(const float *buf, int len)
@@ -214,64 +260,46 @@ int ToneDetector::writeSamples(const float *buf, int len)
  *
  ****************************************************************************/
 
-
-/*
- *----------------------------------------------------------------------------
- * Method:    
- * Purpose:   
- * Input:     
- * Output:    
- * Author:    
- * Created:   
- * Remarks:   
- * Bugs:      
- *----------------------------------------------------------------------------
- */
-
 void ToneDetector::postProcess(void)
 {
-  /* Center frequency */
   float res_center = goertzelResult(&center);
-  /* Lower frequency */
   float res_lower = goertzelResult(&lower);
-  /* Upper frequency */
   float res_upper = goertzelResult(&upper);
 
-  /* Point to the first table entry */
+    // Point to the first table entry
   win = window_table.begin();
-  /* Reload sample counter */
+    // Reload sample counter
   samples_left = block_len;
 
-  if ((res_center > energy_thresh) &&
+  bool active = ((res_center > DEFAULT_ENERGY_THRESH) &&
       (res_center > (res_lower * peak_thresh)) &&
-      (res_center > (res_upper * peak_thresh)))
+      (res_center > (res_upper * peak_thresh)));
+
+  if (active == last_active)
   {
-    if (det_delay_left > 0)
+    stable_count += 1;
+  }
+  else
+  {
+    stable_count = 1;
+  }
+  last_active = active;
+  
+  if (!is_activated)
+  {
+    if (active && (stable_count >= det_delay))
     {
-      if (--det_delay_left == 0)
-      {
-	is_activated = true;
-	activated(true);
-      }
-    }
-    if (is_activated)
-    {
-      undet_delay_left = DEFAULT_UNDET_DELAY;
+      is_activated = true;
+      activated(true);
+      detected(tone_fq);
     }
   }
   else
   {
-    if (undet_delay_left > 0)
+    if (!active && (stable_count >= gap_delay))
     {
-      if (--undet_delay_left == 0)
-      {
-        is_activated = false;
-	activated(false);
-      }
-    }
-    if (!is_activated)
-    {
-      det_delay_left = DEFAULT_DET_DELAY;
+      is_activated = false;
+      activated(false);
     }
   }
 } /* ToneDetector::postProcess */
@@ -279,28 +307,33 @@ void ToneDetector::postProcess(void)
 
 void ToneDetector::goertzelInit(GoertzelState *s, float freq, int sample_rate)
 {
-    s->fac = 2.0f * cosf(2.0f * M_PI * (freq / (float)sample_rate));
-    s->v2 = s->v3 = 0.0f;
+  s->fac = 2.0f * cosf(2.0f * M_PI * (freq / (float)sample_rate));
+    // Reset Goertzel filter
+  s->v2 = s->v3 = 0.0f;
 } /* ToneDetector::goertzelInit */
 
 
 float ToneDetector::goertzelResult(GoertzelState *s)
 {
-    float v1, res;
+  float v1, res;
 
-    /* Push a zero through the process to finish things off. */
-    v1 = s->v2;
-    s->v2 = s->v3;
-    s->v3 = s->fac*s->v2 - v1;
-    /* Now calculate the non-recursive side of the filter. */
-    /* The result here is not scaled down to allow for the magnification
-       effect of the filter (the usual DFT magnification effect). */
-    res = s->v3*s->v3 + s->v2*s->v2 - s->v2*s->v3*s->fac;
-    /* Reset the tone detector state. */
-    s->v2 = s->v3 = 0.0f;
-    /* Return the calculated signal level. */
-    return res;
+    // Push a zero through the process to finish things off
+  v1 = s->v2;
+  s->v2 = s->v3;
+  s->v3 = s->fac * s->v2 - v1;
+
+    // Now calculate the non-recursive side of the filter.
+    // The result here is not scaled down to allow for the magnification
+    // effect of the filter (the usual DFT magnification effect).
+  res = s->v3 * s->v3 + s->v2 * s->v2 - s->v2 * s->v3 * s->fac;
+
+    // Reset Goertzel filter
+  s->v2 = s->v3 = 0.0f;
+
+  return res;
+
 } /* ToneDetector::goertzelResult */
+
 
 /*
  * This file has not been truncated
