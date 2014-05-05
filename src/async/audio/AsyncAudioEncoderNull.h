c71cb24db48243852231ad89e7c51572124a32c3
Tobias Blomberg <sm0svx@users.sourceforge.net>
2014-05-05 20:47:43 +0000

    New NULL audio encoder
    Added a NULL audio encoder that can be used when one does not want audio to be
    sent at all.
    
    
    git-svn-id: svn://svn.code.sf.net/p/svxlink/svn/trunk@2521 f8843c6b-282e-0410-804b-c0d5895947d2

--- src/async/audio/AsyncAudioEncoderNull.h
+++ src/async/audio/AsyncAudioEncoderNull.h
@@ -1,8 +1,8 @@
 /**
-@file	 AudioEncoder.cpp
-@brief   Base class for an audio decoder
+@file	 AsyncAudioEncoderNull.h
+@brief   A null audio "encoder" that just throw away audio
 @author  Tobias Blomberg / SM0SVX
-@date	 2008-10-06
+@date	 2014-05-05
 
 \verbatim
 Async - A library for programming event driven applications
@@ -24,6 +24,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 \endverbatim
 */
 
+#ifndef ASYNC_AUDIO_ENCODER_NULL_INCLUDED
+#define ASYNC_AUDIO_ENCODER_NULL_INCLUDED
 
 
 /****************************************************************************
@@ -40,6 +42,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ****************************************************************************/
 
+#include <AsyncAudioEncoder.h>
 
 
 /****************************************************************************
@@ -48,49 +51,37 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ****************************************************************************/
 
-#include "AsyncAudioEncoder.h"
-#include "AsyncAudioEncoderNull.h"
-#include "AsyncAudioEncoderRaw.h"
-#include "AsyncAudioEncoderS16.h"
-#include "AsyncAudioEncoderGsm.h"
-#ifdef SPEEX_MAJOR
-#include "AsyncAudioEncoderSpeex.h"
-#endif
-#ifdef OPUS_MAJOR
-#include "AsyncAudioEncoderOpus.h"
-#endif
 
 
 /****************************************************************************
  *
- * Namespaces to use
+ * Forward declarations
  *
  ****************************************************************************/
 
-using namespace std;
-using namespace Async;
 
 
+/****************************************************************************
+ *
+ * Namespace
+ *
+ ****************************************************************************/
+
+namespace Async
+{
+
+
+/****************************************************************************
+ *
+ * Forward declarations of classes inside of the declared namespace
+ *
+ ****************************************************************************/
+
+  
 
 /****************************************************************************
  *
  * Defines & typedefs
- *
- ****************************************************************************/
-
-
-
-/****************************************************************************
- *
- * Local class definitions
- *
- ****************************************************************************/
-
-
-
-/****************************************************************************
- *
- * Prototypes
  *
  ****************************************************************************/
 
@@ -104,87 +95,68 @@ using namespace Async;
 
 
 
-
 /****************************************************************************
  *
- * Local Global Variables
+ * Class definitions
  *
  ****************************************************************************/
 
+/**
+@brief	A null audio "encoder" that just throw away audio
+@author Tobias Blomberg / SM0SVX
+@date   2014-05-05
 
-
-/****************************************************************************
- *
- * Public member functions
- *
- ****************************************************************************/
-
-AudioEncoder *AudioEncoder::create(const std::string &name)
+This class implements an audio "encoder" that just throw away samples. This
+can be good to use if audio is sent through another audio path.
+*/
+class AudioEncoderNull : public AudioEncoder
 {
-  if (name == "NULL")
-  {
-    return new AudioEncoderNull;
-  }
-  else if (name == "RAW")
-  {
-    return new AudioEncoderRaw;
-  }
-  else if (name == "S16")
-  {
-    return new AudioEncoderS16;
-  }
-  else if (name == "GSM")
-  {
-    return new AudioEncoderGsm;
-  }
-#ifdef SPEEX_MAJOR
-  else if (name == "SPEEX")
-  {
-    return new AudioEncoderSpeex;
-  }
-#endif
-#ifdef OPUS_MAJOR
-  else if (name == "OPUS")
-  {
-    return new AudioEncoderOpus;
-  }
-#endif
-  else
-  {
-    return 0;
-  }
-} /* AudioEncoder::create */
-
-
-
-#if 0
-AudioEncoder::AudioEncoder(void)
-{
+  public:
+    /**
+     * @brief 	Default constuctor
+     */
+    AudioEncoderNull(void) {}
   
-} /* AudioEncoder::AudioEncoder */
-
-
-AudioEncoder::~AudioEncoder(void)
-{
+    /**
+     * @brief 	Destructor
+     */
+    virtual ~AudioEncoderNull(void) {}
   
-} /* AudioEncoder::~AudioEncoder */
-#endif
+    /**
+     * @brief   Get the name of the codec
+     * @return  Return the name of the codec
+     */
+    virtual const char *name(void) const { return "NULL"; }
+  
+    /**
+     * @brief 	Write samples into this audio sink
+     * @param 	samples The buffer containing the samples
+     * @param 	count The number of samples in the buffer
+     * @return	Returns the number of samples that has been taken care of
+     *
+     * This function is used to write audio into this audio sink. If it
+     * returns 0, no more samples should be written until the resumeOutput
+     * function in the source have been called.
+     * This function is normally only called from a connected source object.
+     */
+    virtual int writeSamples(const float *samples, int count)
+    {
+      return count;
+    }
+    
+    
+  protected:
+    
+  private:
+    AudioEncoderNull(const AudioEncoderNull&);
+    AudioEncoderNull& operator=(const AudioEncoderNull&);
+    
+};  /* class AudioEncoderNull */
 
 
+} /* namespace */
 
-/****************************************************************************
- *
- * Protected member functions
- *
- ****************************************************************************/
-
-
-
-/****************************************************************************
- *
- * Private member functions
- *
- ****************************************************************************/
+#endif /* ASYNC_AUDIO_ENCODER_NULL_INCLUDED */
 
 
 
