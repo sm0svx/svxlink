1d1252e27b45c35ec0d915b0adfa7fc3de041f93
Tobias Blomberg <sm0svx@users.noreply.github.com>
2014-12-20 22:10:18 +0100

    Implement output stream buffer for PTYs

---
+++ src/async/demo/AsyncPtyStreamBuf_demo.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <cstdlib>
+#include <ostream>
+#include <iostream>
+#include <AsyncCppApplication.h>
+#include <AsyncPty.h>
+#include <AsyncPtyStreamBuf.h>
+
+using namespace std;
+
+int main(void)
+{
+  Async::CppApplication app;
+
+  string pty_slave_path("/tmp/testpty");
+  Async::Pty pty(pty_slave_path);
+  if (!pty.open())
+  {
+    cout << "Failed to open PTY " << pty_slave_path << endl;
+    exit(1);
+  }
+
+  Async::PtyStreamBuf psb(pty);
+  ostream os(&psb);
+  os.setf(ios::unitbuf);
+
+  cout << "\nThis demo app will write a line of text every second to a PTY.\n";
+  cout << "To see the printouts, in another console do:\n";
+  cout << "\n\tcat /tmp/testpty\n\n";
+  for (;;)
+  {
+    os << "Hello, PTY!\n";
+    sleep(1);
+  }
+}
+
