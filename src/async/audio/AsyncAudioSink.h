0848fc6b3e447fa2a2b003d9e174d29a4acff558
Richard Neese <r.neese@gmail.com>
2016-12-06 18:57:39 -0600

    fix broken file

--- src/async/audio/AsyncAudioSink.h
+++ src/async/audio/AsyncAudioSink.h
@@ -65,14 +65,9 @@ class AudioSource;
 /*
  * Class definitions
  */
-/**
-@brief	The base class for an audio sink
-@author Tobias Blomberg
-@date   2005-04-17
-
-This is the base class for an audio sink. An audio sink is a class that
-can consume audio.
-*/
+/*
+ * This is the base class for an audio sink. An audio sink is a class that can consume audio.
+ */
 class AudioSink
 {
   public:
