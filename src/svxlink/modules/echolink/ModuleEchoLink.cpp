b0fe51713e022df42c58af0635ffa849baa5fe37
Richard Neese <r.neese@gmail.com>
2016-12-08 14:10:40 -0600

    fix typo in echoloink cpp file

--- src/svxlink/modules/echolink/ModuleEchoLink.cpp
+++ src/svxlink/modules/echolink/ModuleEchoLink.cpp
@@ -35,7 +35,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <sstream>
 #include <cstdlib>
 #include <vector>
-+#include <string.h>
+#include <string.h>
 
 /*
  * Project Includes
