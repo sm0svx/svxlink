381d2b644f59b7263e73c8e09e93133d5ffc55f7
Richard Neese <r.neese@gmail.com>
2016-12-27 19:40:28 -0600

    fix

--- src/systemd/svxlink.logrotate.cmake
+++
@@ -1,9 +0,0 @@
-/var/log/svxlink {
-    missingok
-    notifempty
-    weekly
-    create 0644 svxlink daemon
-    postrotate
-        killall -HUP svxlink
-    endscript
-}
--- src/systemd/svxserver.logrotate.cmake
+++
@@ -1,9 +0,0 @@
-/var/log/svxserver {
-    missingok
-    notifempty
-    weekly
-    create 0644 svxserver daemon
-    postrotate
-        killall -HUP svxlink
-    endscript
-}
