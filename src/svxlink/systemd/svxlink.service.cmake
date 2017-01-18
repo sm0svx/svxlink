239f3329bb211a64bdb32dde61b0a9030a373985
Richard Neese <r.neese@gmail.com>
2017-01-17 19:50:24 -0600

    fix typo

--- src/svxlink/systemd/svxlink.service.cmake
+++ src/svxlink/systemd/svxlink.service.cmake
@@ -13,13 +13,13 @@ ExecStartPre=-/bin/touch /var/log/svxlink
 ExecStartPre=-/bin/chown $RUNASUSER /var/log/svxlink
 ExecStart=/bin/sh -c '@CMAKE_INSTALL_PREFIX@/bin/svxlink --logfile=/var/log/svxlink --config=$CFGFILE --pidfile=/run/svxlink.pid --runasuser=$RUNASUSER'
 ExecReload=/bin/kill -s HUP $MAINPID
-ExecStopPost=@CMAKE_INSTALL_PREFIX@/sbin/svvxlink_gpio_down
+ExecStopPost=@CMAKE_INSTALL_PREFIX@/sbin/svxlink_gpio_down
 Restart=on-failure
 TimeoutStartSec=60
 WatchdogSec=@SVX_WatchdogSec@
 ;;; NotifyAccess=main
 LimitCORE=infinity
-WorkingDirectory=@SVX_SYSCONF_INSTALL_DIIR@
+WorkingDirectory=@SVX_SYSCONF_INSTALL_DIR@
 
 [Install]
 WantedBy=multi-user.target
