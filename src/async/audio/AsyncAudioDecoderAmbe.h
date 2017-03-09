15816e5cfb9d8f837193f3decb76fea3d744b4a9
Adi Bier, DL1HRC <dl1hrc@gmx.de>
2017-03-09 17:44:22 +0100

    configuration, adapted udp messages

---
+++ src/async/audio/AsyncAudioDecoderAmbe.h
@@ -0,0 +1,170 @@
+/**
+@file	 AsyncAudioDecoderAMBE.h
+@brief   An audio decoder that use the AMBE audio codec
+@author  Tobias Blomberg / SM0SVX
+@date	 2013-10-12
+
+\verbatim
+Async - A library for programming event driven applications
+Copyright (C) 2003-2013 Tobias Blomberg / SM0SVX
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+\endverbatim
+*/
+
+#ifndef ASYNC_AUDIO_DECODER_AMBE_INCLUDED
+#define ASYNC_AUDIO_DECODER_AMBE_INCLUDED
+
+
+/****************************************************************************
+ *
+ * System Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Project Includes
+ *
+ ****************************************************************************/
+
+#include <AsyncAudioDecoder.h>
+
+
+/****************************************************************************
+ *
+ * Local Includes
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Forward declarations
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Namespace
+ *
+ ****************************************************************************/
+
+namespace Async
+{
+
+
+/****************************************************************************
+ *
+ * Forward declarations of classes inside of the declared namespace
+ *
+ ****************************************************************************/
+
+  
+
+/****************************************************************************
+ *
+ * Defines & typedefs
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Exported Global Variables
+ *
+ ****************************************************************************/
+
+
+
+/****************************************************************************
+ *
+ * Class definitions
+ *
+ ****************************************************************************/
+
+/**
+@brief	An audio decoder that use the AMBE audio codec
+@author Tobias Blomberg / SM0SVX
+@date   2013-10-12
+
+This class implements an audio decoder that use the AMBE audio codec.
+*/
+class AudioDecoderAmbe : public AudioDecoder
+{
+  public:
+    /**
+     * @brief 	Default constuctor
+     */
+    AudioDecoderAmbe(void);
+  
+    /**
+     * @brief 	Destructor
+     */
+    virtual ~AudioDecoderAmbe(void);
+  
+    /**
+     * @brief   Get the name of the codec
+     * @returns Return the name of the codec
+     */
+    virtual const char *name(void) const { return "AMBE"; }
+  
+    /**
+     * @brief 	Set an option for the decoder
+     * @param 	name The name of the option
+     * @param 	value The value of the option
+     */
+    virtual void setOption(const std::string &name, const std::string &value);
+
+    /**
+     * @brief   Resets encoder to be equivalent to a freshly initialized one
+     */
+    void reset(void);
+
+    /**
+     * @brief 	Write encoded samples into the decoder
+     * @param 	buf  Buffer containing encoded samples
+     * @param 	size The size of the buffer
+     */
+    virtual void writeEncodedSamples(void *buf, int size);
+    
+
+  protected:
+    
+  private:
+    int         frame_size;
+    
+    AudioDecoderAmbe(const AudioDecoderAmbe&);
+    AudioDecoderAmbe& operator=(const AudioDecoderAmbe&);
+    
+};  /* class AudioDecoderAMBE */
+
+
+} /* namespace */
+
+#endif /* ASYNC_AUDIO_DECODER_AMBE_INCLUDED */
+
+
+
+/*
+ * This file has not been truncated
+ */
+
