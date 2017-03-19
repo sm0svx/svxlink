08244601633f4763adb31e69d9a196fb68de9197
Tobias Blomberg <sm0svx@users.noreply.github.com>
2017-03-19 20:03:06 +0100

    Forgot one file in the previous commit

--- src/svxlink/trx/WbRxRtlSdr.cpp
+++ src/svxlink/trx/WbRxRtlSdr.cpp
@@ -176,7 +176,7 @@ WbRxRtlSdr::WbRxRtlSdr(Async::Config &cfg, const string &name)
   rtl->readyStateChanged.connect(
       mem_fun(*this, &WbRxRtlSdr::rtlReadyStateChanged));
 
-  uint32_t fq_corr = 0;
+  int fq_corr = 0;
   if (cfg.getValue(name, "FQ_CORR", fq_corr) && (fq_corr != 0))
   {
     //cout << "###   FQ_CORR     = " << (int32_t)fq_corr << "ppm\n";
