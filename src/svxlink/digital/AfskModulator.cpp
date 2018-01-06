804f6a2849cf25950e0dcbae824692ae1714af89
Tobias Blomberg <sm0svx@users.noreply.github.com>
2018-01-06 23:27:34 +0100

    Remove remez code

--- src/svxlink/digital/AfskModulator.cpp
+++ src/svxlink/digital/AfskModulator.cpp
@@ -53,7 +53,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  ****************************************************************************/
 
-#include "remez.h"
+//#include "remez.h"
 #include "AfskModulator.h"
 
 
