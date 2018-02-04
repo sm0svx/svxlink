25b89e742ebaf967ba2868ec6d7b194571048385
Daniel Loranger <dloranger08@gmail.com>
2018-02-04 11:00:17 -0800

    Adding TIME_FORMAT config variable
    - New config variable to support 12/24 hour time
    - Updating the docs to add new config option and many spelling fixes

--- src/svxlink/svxlink/locale.tcl
+++ src/svxlink/svxlink/locale.tcl
@@ -138,6 +138,8 @@ proc playNumber {number} {
 # Say the time specified by function arguments "hour" and "minute".
 #
 proc playTime {hour minute} {
+  variable Logic::CFG_TIME_FORMAT
+
   # Strip white space and leading zeros. Check ranges.
   if {[scan $hour "%d" hour] != 1 || $hour < 0 || $hour > 23} {
     error "playTime: Non digit hour or value out of range: $hour"
@@ -145,30 +147,47 @@ proc playTime {hour minute} {
   if {[scan $minute "%d" minute] != 1 || $minute < 0 || $minute > 59} {
     error "playTime: Non digit minute or value out of range: $hour"
   }
-  
-  if {$hour < 12} {
-    set ampm "AM";
-    if {$hour == 0} {
-      set hour 12;
-    }
-  } else {
-    set ampm "PM";
-    if {$hour > 12} {
-      set hour [expr $hour - 12];
-    }
-  };
-  
-  playMsg "Default" [expr $hour];
 
-  if {$minute != 0} {
-    if {[string length $minute] == 1} {
-      set minute "O$minute";
+  if {[info exists CFG_TIME_FORMAT] && ($CFG_TIME_FORMAT == 24)} {
+    if {$hour == 0} {
+      set hour "00"
+    } elseif {[string length $hour] == 1} {
+      set hour "o$hour";
     }
-    playTwoDigitNumber $minute;
-  }
+    playTwoDigitNumber $hour;
+
+    if {$minute != 0} {
+      if {[string length $minute] == 1} {
+        set minute "o$minute";
+      }
+      playTwoDigitNumber $minute;
+    }
+    playMsg "Default" "hours";
+    playSilence 100;
+  } else {
+    # Anything not 24 will fail back to 12 hour default
+    if {$hour < 12} {
+      set ampm "AM";
+      if {$hour == 0} {
+        set hour 12;
+      }
+    } else {
+      set ampm "PM";
+      if {$hour > 12} {
+        set hour [expr $hour - 12];
+      }
+    }
   
-  playSilence 100;
-  playMsg "Core" $ampm;
+    playMsg "Default" [expr $hour];
+    if {$minute != 0} {
+      if {[string length $minute] == 1} {
+        set minute "o$minute";
+      }
+      playTwoDigitNumber $minute;
+    }
+    playSilence 100;
+    playMsg "Core" $ampm;
+  }
 }
 
 
