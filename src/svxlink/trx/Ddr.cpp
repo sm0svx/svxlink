1bd9c9ac8655e0e4f573ca3207ebdf5108cba33f
Tobias Blomberg <sm0svx@users.noreply.github.com>
2019-02-24 11:58:33 +0100

    Remove unused include file

--- src/svxlink/trx/Ddr.cpp
+++ src/svxlink/trx/Ddr.cpp
@@ -58,7 +58,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <AsyncConfig.h>
 #include <AsyncAudioSource.h>
 #include <AsyncTcpClient.h>
-#include <AsyncThreadSigCAsyncConnector.h>
 #include <AsyncAudioThreadSource.h>
 #include <AsyncThreadSigCSignal.h>
 
