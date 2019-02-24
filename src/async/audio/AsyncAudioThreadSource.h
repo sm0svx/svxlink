16b4319696fdc8f99127f594deff92f58444e5c3
Tobias Blomberg <sm0svx@users.noreply.github.com>
2019-02-24 20:36:31 +0100

    Async::AudioThreadSource implementation cleanup

--- src/async/audio/AsyncAudioThreadSource.h
+++ src/async/audio/AsyncAudioThreadSource.h
@@ -1,6 +1,6 @@
 /**
-@file   AsyncAudioThreadFifo.h
-@brief  An audio fifo used to safely connect two different threads
+@file   AsyncAudioThreadSource.h
+@brief  A threadsafe audio source base class
 @author Tobias Blomberg / SM0SVX
 @date   2019-02-02
 
@@ -23,11 +23,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 \endverbatim
 */
-
-/** @example AudioThreadSource_demo.cpp
-An example of how to use the Async::AudioThreadSource class
-*/
-
 
 #ifndef ASYNC_AUDIO_THREAD_SOURCE_INCLUDED
 #define ASYNC_AUDIO_THREAD_SOURCE_INCLUDED
@@ -54,7 +49,6 @@ An example of how to use the Async::AudioThreadSource class
 #include <AsyncApplication.h>
 #include <AsyncAudioSink.h>
 #include <AsyncAudioSource.h>
-#include <AsyncMutex.h>
 
 
 /****************************************************************************
@@ -114,13 +108,13 @@ namespace Async
  ****************************************************************************/
 
 /**
-@brief  A_brief_class_description
+@brief  A threadsafe audio source base class
 @author Tobias Blomberg / SM0SVX
 @date   2019-02-02
 
-A_detailed_class_description
-
-\include AudioThreadSource_demo.cpp
+This is a base class for writing threadsafe audio source classes. This is of
+use when the audio source is running in a separate thread, not in the Async
+main thread.
 */
 class AudioThreadSource : public Async::AudioSource
 {
@@ -162,11 +156,8 @@ class AudioThreadSource : public Async::AudioSource
         m_flush = false;
         m_in_buf.insert(m_in_buf.end(), samples, samples+count);
       }
-      {
-        std::lock_guard<Async::Mutex> lk(m_mu);
-        Async::Application::app().runTask(
-            sigc::mem_fun(*this, &AudioThreadSource::resumeOutput));
-      }
+      Async::Application::app().runTask(
+          sigc::mem_fun(*this, &AudioThreadSource::resumeOutput));
       return count;
     }
 
@@ -179,27 +170,28 @@ class AudioThreadSource : public Async::AudioSource
      */
     virtual void sinkFlushSamples(void)
     {
-      std::lock_guard<Async::Mutex> lk(m_mu);
-      m_flush = true;
+      {
+        std::lock_guard<std::mutex> lk(m_in_buf_mu);
+        m_flush = true;
+      }
       Async::Application::app().runTask(
           sigc::mem_fun(*this, &AudioThreadSource::resumeOutput));
     }
 
     void waitForAllSamplesFlushed(void)
     {
-      std::unique_lock<Async::Mutex> lk(m_mu);
+      std::unique_lock<std::mutex> lk(m_all_flushed_mu);
       m_all_flushed_cond.wait(lk, [this]{ return m_all_flushed; });
     }
 
   private:
-    Async::Mutex                  m_mu;
     std::mutex                    m_in_buf_mu;
     std::vector<float>            m_in_buf;
     std::vector<float>            m_out_buf;
     std::vector<float>::iterator  m_out_buf_it = m_out_buf.begin();
     bool                          m_flush = false;
-    bool                          m_unflushed = false;
     bool                          m_all_flushed = true;
+    std::mutex                    m_all_flushed_mu;
     std::condition_variable_any   m_all_flushed_cond;
 
     /**
@@ -207,7 +199,7 @@ class AudioThreadSource : public Async::AudioSource
      *
      * This function will be called when the registered audio sink is ready
      * to accept more samples. It is also called when more samples have been
-     * written to the queue or if the flushSamples function is called.
+     * written to the queue or if the sinkFlushSamples function is called.
      */
     virtual void resumeOutput(void)
     {
@@ -223,15 +215,15 @@ class AudioThreadSource : public Async::AudioSource
         }
         if (!m_out_buf.empty())
         {
-          m_unflushed = true;
-          int cnt = AudioSource::sinkWriteSamples(&(*m_out_buf_it),
-              m_out_buf.end()-m_out_buf_it);
+          int write_cnt = m_out_buf.end()-m_out_buf_it;
+          int cnt = AudioSource::sinkWriteSamples(&(*m_out_buf_it), write_cnt);
           if (cnt > 0)
           {
-            m_out_buf_it += cnt;
+            m_out_buf_it += std::min(cnt, write_cnt);
             if (m_out_buf_it == m_out_buf.end())
             {
               m_out_buf.clear();
+              m_out_buf_it = m_out_buf.begin();
             }
           }
           else
@@ -247,12 +239,11 @@ class AudioThreadSource : public Async::AudioSource
               std::lock_guard<std::mutex> lk(m_in_buf_mu);
               m_flush = false;
             }
-            if (m_unflushed)
             {
-              m_unflushed = false;
+              std::lock_guard<std::mutex> lk(m_all_flushed_mu);
               m_all_flushed = false;
-              AudioSource::sinkFlushSamples();
             }
+            AudioSource::sinkFlushSamples();
           }
           break;
         }
@@ -268,7 +259,10 @@ class AudioThreadSource : public Async::AudioSource
      */
     virtual void allSamplesFlushed(void)
     {
-      m_all_flushed = true;
+      {
+        std::lock_guard<std::mutex> lk(m_all_flushed_mu);
+        m_all_flushed = true;
+      }
       m_all_flushed_cond.notify_all();
     }
 
