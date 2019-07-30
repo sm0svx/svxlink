9397cb29b7567ba483271aa4fb417da4241df10a
Tobias Blomberg <sm0svx@users.noreply.github.com>
2019-07-31 01:16:42 +0200

    Add missing header files

--- src/locationinfo/AprsTcpClient.cpp
+++ src/locationinfo/AprsTcpClient.cpp
@@ -35,6 +35,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <iostream>
 #include <cmath>
 #include <cstring>
+#include <sys/time.h>
 
 
 /****************************************************************************
