34d5638960c75abb7e1943a59f8c89c3afae3845
Adi Bier / DL1HRC <dl1hrc@gmx.de>
2020-11-19 12:47:43 +0100

    Bugfix: Calculation of positions west of the null meridian

--- src/svxlink/svxlink/TetraLib.h
+++ src/svxlink/svxlink/TetraLib.h
@@ -40,7 +40,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
 #include <math.h>
 
 
@@ -102,13 +102,13 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * Exported Global Variables
  *
  ****************************************************************************/
- 
+
 /* TxGrant
-   This parameter is used in the call maintenance phase of simplex circuit 
-   mode calls. When received from the MT the TE will know what to do with 
+   This parameter is used in the call maintenance phase of simplex circuit
+   mode calls. When received from the MT the TE will know what to do with
    the transmitter on the established circuit.
 */
-std::string TxGrant[] = { 
+std::string TxGrant[] = {
    "0 - Transmission granted",
    "1 - Transmission not granted",
    "2 - Transmission queued",
@@ -116,7 +116,7 @@ std::string TxGrant[] = {
 };
 
 /* Call status
-   The call status information element shall inform the MS/LS about the 
+   The call status information element shall inform the MS/LS about the
    status of the call
 */
 std::string CallStatus[] = {
@@ -137,12 +137,12 @@ std::string CalledPartyIdentityType[] = {
 };
 
 std::string OpMode[] = {
-   "0 - V+D (trunked mode operation)", 
-   "1 - DMO", 
+   "0 - V+D (trunked mode operation)",
+   "1 - DMO",
    "2 - V+D with dual watch of DMO",
-   "3 - DMO with dual watch of V+D", 
-   "4 - V+D and DMO (used in conjunction CTSP command)", 
-   "5 - NN", 
+   "3 - DMO with dual watch of V+D",
+   "4 - V+D and DMO (used in conjunction CTSP command)",
+   "5 - NN",
    "6 - DMO Repeater mode"
 };
 
@@ -151,14 +151,14 @@ std::string OpMode[] = {
    the importance of a TX-Demand
 */
 std::string TxDemandPriority[] = {
-   "0 - Low", 
-   "1 - High", 
-   "2 - Pre-emptive", 
+   "0 - Low",
+   "1 - High",
+   "2 - Pre-emptive",
    "3 - Emergency"
 };
 
 /* Transient communication type
-   This parameter is used to indicate the communication type in either 
+   This parameter is used to indicate the communication type in either
    V+D or DMO.
 */
 std::string TransientComType[] = {
@@ -166,11 +166,11 @@ std::string TransientComType[] = {
    "1 - DMO-Direct MS-MS",
    "2 - DMO-Via DM-REP",
    "3 - DMO-Via DM-GATE",
-   "4 - DMO-Via DM-REP/GATE" 
+   "4 - DMO-Via DM-REP/GATE"
 };
 
 /* Reg stat
-   This parameter is used to indicate the registration status of the MT to the 
+   This parameter is used to indicate the registration status of the MT to the
    TEI.
 */
 std::string RegStat[] = {
@@ -183,7 +183,7 @@ std::string RegStat[] = {
 };
 
 /* AI Service
-   This parameter is used to determine the type of service to be used in air 
+   This parameter is used to determine the type of service to be used in air
    interface call set up signalling.
 */
 std::string AiService[] = {
@@ -204,7 +204,7 @@ std::string AiService[] = {
 };
 
 /* AI mode
-   This parameter is used to indicate the mode of operation or the air 
+   This parameter is used to indicate the mode of operation or the air
    interface protocol stack.
 */
 std::string AiMode[] = {
@@ -216,9 +216,9 @@ std::string AiMode[] = {
 };
 
 /* Disconnect cause
-   This parameter is given in the disconnect message from the MT 
+   This parameter is given in the disconnect message from the MT
    when a circuit-mode call is cleared by the other end, the
-   SwMI, the gateway (in the case of DMO) or the MT itself. The 
+   SwMI, the gateway (in the case of DMO) or the MT itself. The
    latter may have been requested by the TE. The TE could
    use the information in MMI or to initiate retries.
 */
@@ -267,9 +267,9 @@ std::string DisconnectCause[] = {
 };
 
 /* DM communication type
-   This parameter is used to indicate the communication type 
+   This parameter is used to indicate the communication type
    for outgoing calls in DMO, i.e. whether it is a direct
-   communication between MSs, or whether it is being routed 
+   communication between MSs, or whether it is being routed
    via a DM-REP, DM-GATE or DM-REP/GATE.
 */
 std::string DmCommunicationType[] = {
@@ -294,9 +294,9 @@ std::string TetraNumType[] = {
 };
 
 /* Num type
-   This parameter is used to indicate the type of identity returned by the 
-   +CNUM command. The service centre is used as (an optional) part of the 
-   SDS-TL transport service protocol. The gateways returned are those used in 
+   This parameter is used to indicate the type of identity returned by the
+   +CNUM command. The service centre is used as (an optional) part of the
+   SDS-TL transport service protocol. The gateways returned are those used in
    the SwMI for access to PSTN and PABX services.
 */
 std::string NumType[] = {
@@ -336,10 +336,10 @@ std::string ReasonForSending[] = {
 };
 
 /* Group type
-   This parameter is used when setting the MT groups for use in V+D. A 
+   This parameter is used when setting the MT groups for use in V+D. A
    selected group will be used for outgoing calls.
-   Either selected or scanned groups will receive incoming calls. Only 
-   incoming group calls with a priority higher than the scan level will 
+   Either selected or scanned groups will receive incoming calls. Only
+   incoming group calls with a priority higher than the scan level will
    interrupt ongoing group calls of a lower level.
    If the group type is "none" all groups will be detached from the SwMI.
 */
@@ -354,7 +354,7 @@ std::string GroupType[] = {
   "7 - Scan priority 6"
 };
 
-// This parameter enables the unsolicited reporting of changes in visible 
+// This parameter enables the unsolicited reporting of changes in visible
 // gateways and/or repeaters.
 std::string sdsStatus[] {
   "0 - Incoming message stored and unread",
@@ -389,22 +389,26 @@ unsigned short msgcnt;
 std::string dec2nmea_lat(float latitude)
 {
   char lat[10];
-  float minute = (latitude - int(latitude)) * 60;
-  sprintf(lat, "%02d%05.2f", int(latitude), minute);
-  std::string s = std::string(lat);
-  s += (latitude > 0 ? "N": "S");
-  return s;
+  float degrees;
+  float normalizedLat = std::fmod(latitude, 180.0f);
+  float fractpart = std::modf(std::abs(normalizedLat), &degrees);
+  float minute = fractpart * 60.0;
+  char dir = normalizedLat > 0.0 ? 'N' : 'S';
+  sprintf(lat, "%02.0f%05.2f%c", degrees, minute, dir);
+  return std::string(lat);;
 } /* dec2nmea_lat */
 
 
 std::string dec2nmea_lon(float longitude)
 {
   char lon[10];
-  float minute = (longitude - int(longitude)) * 60;
-  sprintf(lon, "%03d%05.2f", int(longitude), minute);
-  std::string s = std::string(lon);
-  s += (longitude > 0 ? "E": "W");
-  return s;
+  float degrees;
+  float normalizedLon = std::fmod(longitude, 360.0f);
+  float fractpart = std::modf(std::abs(normalizedLon), &degrees);
+  float minute = fractpart * 60.0;
+  char dir = normalizedLon > 0.0 ? 'E' : 'W';
+  sprintf(lon, "%03.0f%05.2f%c", degrees, minute, dir);
+  return std::string(lon);
 } /* dec2nmea_lon */
 
 
@@ -448,15 +452,16 @@ void handleLipSds(std::string in, LipInfo &lipinfo)
      0x83 = Complex SDS-TL GPS message transfer
   */
   double tla;
-  double tlo; 
+  double tlo;
 
   int t_velo;
   int t_dot;
-  
-  //4
+
+  //
   if (in.substr(0,2) == "0A") // LIP
   {
     lipinfo.time_elapsed = (std::stoi(in.substr(2,1),nullptr,16) & 0x03);
+
     tlo =  std::stol(in.substr(3,1),nullptr,16) << 21;
     tlo += std::stol(in.substr(4,1),nullptr,16) << 17;
     tlo += std::stol(in.substr(5,1),nullptr,16) << 13;
@@ -464,7 +469,7 @@ void handleLipSds(std::string in, LipInfo &lipinfo)
     tlo += std::stol(in.substr(7,1),nullptr,16) << 5;
     tlo += std::stol(in.substr(8,1),nullptr,16) << 1;
     tlo += (std::stol(in.substr(9,1),nullptr,16) & 0x08) >> 3;
-    
+
     tla = (std::stol(in.substr(9,1),nullptr,16) & 0x07) << 21;
     tla += std::stol(in.substr(10,1),nullptr,16) << 17;
     tla += std::stol(in.substr(11,1),nullptr,16) << 13;
@@ -473,14 +478,31 @@ void handleLipSds(std::string in, LipInfo &lipinfo)
     tla += std::stol(in.substr(14,1),nullptr,16) << 1;
     tla += (std::stol(in.substr(15,1),nullptr,16) & 0x08) >> 3;
 
-    lipinfo.latitude = tla * 360 / 33554432;
-    lipinfo.longitude = tlo * 360 / 33554432;
-  
+    if (tlo > 8388608)
+    {
+      lipinfo.longitude = tlo * 360.0 / 33554432 - 360.0;
+    }
+    else
+    {
+      lipinfo.longitude = tlo * 360.0 / 33554432;
+    }
+    std::cout << "tlo=" << tlo << ",lipinfo.longitude=" << lipinfo.longitude << std::endl;
+
+    if (tla > 8388608)
+    {
+      lipinfo.latitude = tla * 360.0 / 33554432 - 360.0;
+    }
+    else
+    {
+      lipinfo.latitude = tla * 360.0 / 33554432;
+    }
+    std::cout << "tla=" << tla << ",lipinfo.latitude=" << lipinfo.latitude << std::endl;
+
     // position error in meter
     lipinfo.positionerror = 2*pow(10,(std::stoi(in.substr(15,1),nullptr,16) & 0x03));
-    
+
     /*
-      Horizontal velocity shall be encoded for speeds 0 km/h to 28 km/h in 1 km/h 
+      Horizontal velocity shall be encoded for speeds 0 km/h to 28 km/h in 1 km/h
       steps and from 28 km/h onwards using equation:
       v = C × (1 + x)^(K-A) + B where:
         • C = 16
@@ -499,17 +521,17 @@ void handleLipSds(std::string in, LipInfo &lipinfo)
     {
       lipinfo.horizontalvelocity =  16 * pow(1.038, (t_velo - 13));
     }
-     
+
     /*
      definition can be expressed also by equation:
-     Direction of travel value = trunc((direction + 11,25)/22,5), when 
+     Direction of travel value = trunc((direction + 11,25)/22,5), when
      direction is given in degrees.
     */
     t_dot = (std::stoi(in.substr(17,1),nullptr,16) & 0x01) << 3;
     t_dot += (std::stoi(in.substr(18,1),nullptr,16) & 0x0e) >> 1;
     lipinfo.directionoftravel = t_dot * 22.5;
-    
-    /* 
+
+    /*
     reason for sending
     */
     lipinfo.reasonforsending = std::stoi(in.substr(19,1),nullptr, 16);
@@ -547,7 +569,7 @@ bool createSDS(std::string & sds, std::string issi, std::string message)
   if (message.length() > 120 || issi.length() > 8) return false;
 
   std::stringstream ss;
-  ss << "8204" << std::setfill('0') << std::setw(sizeof(short)) 
+  ss << "8204" << std::setfill('0') << std::setw(sizeof(short))
      << std::hex << getSdsMesgId() << "01";
 
   for (unsigned int a=0; a<message.length(); a++)
@@ -581,9 +603,9 @@ bool createStateSDS(std::string & sds, std::string issi)
 {
   if (issi.length() > 8) return false;
   std::stringstream ss;
-  ss << "821000" << std::setfill('0') << std::setw(sizeof(short)) 
+  ss << "821000" << std::setfill('0') << std::setw(sizeof(short))
      << std::hex << getSdsMesgId();
-     
+
   char f[issi.length() + ss.str().length()+20];
   sprintf(f, "AT+CMGS=%s,%03d\r\n%s%c",
              std::to_string(std::stoi(issi)).c_str(),
@@ -631,8 +653,8 @@ float calcDistance(float lat1, float lon1, float lat2, float lon2)
 
   double dlon = M_PI * (lon2 - lon1) / 180.0;
   double dlat = M_PI * (lat2 - lat1) / 180.0;
-  
-  double a = (sin(dlat / 2) * sin(dlat / 2)) + cos(M_PI*lat1/180.0) * 
+
+  double a = (sin(dlat / 2) * sin(dlat / 2)) + cos(M_PI*lat1/180.0) *
               cos(M_PI*lat2/180) * (sin(dlon / 2) * sin(dlon / 2));
   double angle = 2 * atan2(sqrt(a), sqrt(1 - a));
   return static_cast<float>(static_cast<int>(angle * RADIUS * 100.))/100.;
@@ -723,7 +745,7 @@ transaction requiring a response from the MS or the SwMI. In DMO, the\n \
 signalling activity could be e.g. Call or SDS processing.",
 "43 - The MT supports the requested service but not while it is in V+D",
 "44 - The MT supports handling of unknown parameters",
-"45 - Reserved" 
+"45 - Reserved"
 };
 
 return error[errorcode];
