b27724df22e3e3cd713f08c28acb9e4fbe8d86fc
Tobias Blomberg <sm0svx@users.noreply.github.com>
2020-11-28 00:32:43 +0100

    Extended support for changing variables at runtime
    - Now possible to set configuration variables from TCL using the
      setConfigValue function. This can be used to change certain configuration of
      SvxLink while it is running. Only some configuration variables support being
      changed at runtime.
    - If a configuration variable is changed in the logic core, or in the main
      configuration section for a module, the new TCL function config_updated will
      be called.
    - New configuration variable ONLINE that can be used to decide if a logic core
      should be active or not on program startup. This variable also support being
      set at runtime.
    - Use the new configuration variable COMMAND_PTY to set up a PTY that can be
      used to send commands to a logic core.

--- src/svxlink/svxlink/EventHandler.h
+++ src/svxlink/svxlink/EventHandler.h
@@ -225,7 +225,17 @@ class EventHandler : public sigc::trackable
      * @param 	duration  The duration of each digit in milliseconds
      */
     sigc::signal<void, const std::string&, int> injectDtmf;
-    
+
+    /**
+     * @brief 	A signal that is emitted when the TCL script want to set
+     *	      	a configuration variable
+     * @param 	section The name of the configuration section
+     * @param 	tag     The name of the configureation variable
+     * @param 	value   The new value for the configuration variable
+     */
+    sigc::signal<void, const std::string&, const std::string&,
+                 const std::string&> setConfigValue;
+
   protected:
 
   private:
@@ -248,6 +258,8 @@ class EventHandler : public sigc::trackable
     static int playDtmfHandler(ClientData cdata, Tcl_Interp *irp,
                     int argc, const char *argv[]);
     static int injectDtmfHandler(ClientData cdata, Tcl_Interp *irp,
+                    int argc, const char *argv[]);
+    static int setConfigValueHandler(ClientData cdata, Tcl_Interp *irp,
                     int argc, const char *argv[]);
 
 };  /* class EventHandler */
