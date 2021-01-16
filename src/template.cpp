14a3f205f93e28d875968a50eec208be733ad631
Tobias Blomberg <sm0svx@users.noreply.github.com>
2021-01-16 23:30:31 +0100

    Update source code templates

--- src/template.cpp
+++ src/template.cpp
@@ -2,13 +2,13 @@
 @file   MyNamespaceTemplate.cpp
 @brief  A_brief_description_for_this_file
 @author Tobias Blomberg / SM0SVX
-@date   2020-
+@date   2021-
 
 A_detailed_description_for_this_file
 
 \verbatim
 <A brief description of the program or library this file belongs to>
-Copyright (C) 2003-2020 Tobias Blomberg / SM0SVX
+Copyright (C) 2003-2021 Tobias Blomberg / SM0SVX
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
