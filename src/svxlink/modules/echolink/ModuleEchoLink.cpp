7a84675de7ecd242a2ad91ecb4f0808ec674f930
Tobias Blomberg <sm0svx@users.noreply.github.com>
2025-10-12 14:33:13 +0200

    Remove debug printouts in ModuleEchoLink

--- src/svxlink/modules/echolink/ModuleEchoLink.cpp
+++ src/svxlink/modules/echolink/ModuleEchoLink.cpp
@@ -2075,7 +2075,6 @@ bool ModuleEchoLink::numConCheck(const std::string &callsign)
   if (cit != num_con_map.end())
   {
       // Get an alias (reference) to the callsign and NumConStn objects
-    const string &t_callsign = (*cit).first;
     NumConStn &stn = (*cit).second;
 
       // Calculate time difference from last connection
@@ -2088,8 +2087,9 @@ bool ModuleEchoLink::numConCheck(const std::string &callsign)
     {
       ++stn.num_con;
       stn.last_con = con_time;
-      cout << "### Station " << t_callsign << ", count " << stn.num_con << " of "
-           << num_con_max << " possible number of connects" << endl;
+      //const string &t_callsign = (*cit).first;
+      //cout << "### Station " << t_callsign << ", count " << stn.num_con << " of "
+      //     << num_con_max << " possible number of connects" << endl;
     }
 
       // Number of connects are too high
@@ -2109,8 +2109,8 @@ bool ModuleEchoLink::numConCheck(const std::string &callsign)
   else
   {
       // Insert initial entry on first connect
-    cout << "### Register incoming station, count 1 of " << num_con_max
-         << " possible number of connects" << endl;
+    //cout << "### Register incoming station, count 1 of " << num_con_max
+    //     << " possible number of connects" << endl;
     num_con_map.insert(make_pair(callsign, NumConStn(1, con_time)));
   }
 
@@ -2129,7 +2129,6 @@ void ModuleEchoLink::numConUpdate(void)
   while (cit != num_con_map.end())
   {
       // Get an alias (reference) to the callsign and NumConStn objects
-    const string &t_callsign = (*cit).first;
     const NumConStn &stn = (*cit).second;
 
     struct timeval remove_at = stn.last_con;
@@ -2145,15 +2144,16 @@ void ModuleEchoLink::numConUpdate(void)
       // If the entry have timed out, delete it
     if (timercmp(&remove_at, &now, <))
     {
-      cout << "### Delete " << t_callsign << " from watchlist" << endl;
+      //const string &t_callsign = (*cit).first;
+      //cout << "### Delete " << t_callsign << " from watchlist" << endl;
       num_con_map.erase(cit++);
     }
     else
     {
-      if (stn.num_con > num_con_max)
-      {
-        cout << "### " << t_callsign << " is blocked" << endl;
-      }
+      //if (stn.num_con > num_con_max)
+      //{
+      //  cout << "### " << t_callsign << " is blocked" << endl;
+      //}
       ++cit;
     }
   }
